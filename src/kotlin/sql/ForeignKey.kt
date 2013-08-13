6c94d77a88cc8fe5822dbc0034014d6a1d2a7241
Maxim Shafirov <max@jetbrains.com>
2013-08-13 17:45:29 +0400

    deadcode

--- src/kotlin/sql/ForeignKey.kt
+++
@@ -1,5 +0,0 @@
-package kotlin.sql
-
-class ForeignKey(val table:Table, val column:Column<*>, val referencedTable:Table) {
-    val name = "fk_${table.tableName}_${referencedTable.tableName}_${column.name}"
-}
