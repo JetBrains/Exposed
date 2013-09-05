8ec95180570dd62d36917f4863aebd673406448b
Maxim Shafirov <max@jetbrains.com>
2013-09-05 13:29:30 +0400

    Exposed logging

--- src/kotlin/sql/SQLLog.kt
+++ src/kotlin/sql/SQLLog.kt
@@ -1,7 +1,11 @@
 package kotlin.sql
 
+import org.apache.log4j.Logger
+
+private val logger = Logger.getLogger("Exposed")
+
 fun log(stmt: String) {
-//    println(stmt)
+    logger?.debug(stmt)
 }
 
 fun log(stmt: StringBuilder) {
