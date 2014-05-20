a7b34989e0ab977caef2f8ed452bd1a331472e39
qx <sergeyQx@gmail.com>
2014-05-20 13:39:10 +0400

    JPF-231 Fix import of D367451248

--- src/kotlin/dao/HistoryEntity.kt
+++ src/kotlin/dao/HistoryEntity.kt
@@ -6,7 +6,7 @@ public open class HistoryEntity (id: EntityID, table: HistoryTable) : Entity(id)
     public var start: DateTime by table.start
     public var end: DateTime? by table.end
 
-    public fun isValidBy (date: DateTime? = null) : Boolean {
+    public open fun isValidBy (date: DateTime? = null) : Boolean {
         if (date == null) return end == null
         return start <= date && (end == null || date < end!!)
     }
