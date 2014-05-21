0e425fd40a658117b434a21a4288efc8572cf5b7
qx <sergeyQx@gmail.com>
2014-05-21 16:56:10 +0400

    + fixed Entity.warmUpCache + no redundand queries in CustomersSearch

--- src/kotlin/dao/Entity.kt
+++ src/kotlin/dao/Entity.kt
@@ -401,7 +401,7 @@ abstract public class EntityClass<out T: Entity>(val table: IdTable, val eagerSe
     private fun warmCache(): EntityCache {
         val cache = EntityCache.getOrCreate(Session.get())
         if (eagerSelect) {
-            if (!cache.data.containsKey(this)) {
+            if (!cache.data.containsKey(table)) {
                 retrieveAll().reduce { a, b -> a }
             }
         }
