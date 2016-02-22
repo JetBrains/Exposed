09fa26c3a36ba075e03761d221980582ce79154e
Kviring Aleksey <kviring@alternativaplatform.com>
2016-02-22 18:43:42 +0500

    Add PostgreSQL dialect (base functional) Support Sequence for autoincremental column

---
+++ src/main/kotlin/org/jetbrains/exposed/sql/vendors/PostgreSQL.kt
@@ -0,0 +1,45 @@
+package org.jetbrains.exposed.sql.vendors
+
+import org.h2.jdbc.JdbcConnection
+import org.jetbrains.exposed.sql.*
+import java.util.*
+
+/**
+ * User: Kviring Aleksey
+ * Date: 21.03.2016
+ */
+
+internal object PostgreSQL : VendorDialect("postgresql") {
+
+    companion object {
+        val DEFAULT_SEQ = "id_seq"
+    }
+
+
+    override fun configure(database: Database): Unit {
+        createSequenceIfNotExists(database, DEFAULT_SEQ)
+    }
+
+    private fun createSequenceIfNotExists(database: Database, sequence: String) {
+        database.transaction {
+            var exists_sequence = HashSet<String>()
+            exec("SELECT sequence_name FROM information_schema.sequences") {
+                while (it.next()) {
+                    exists_sequence.add(it.getString("sequence_name"))
+                }
+            }
+            if (!exists_sequence.contains(sequence)) {
+                exec("create sequence $sequence") {}
+            }
+        }
+    }
+
+    override fun getDatabase(): String {
+        return Transaction.current().connection.catalog
+    }
+
+    override fun supportsSelectForUpdate() = true
+    override fun shortAutoincType(): String = "INT DEFAULT nextval('$DEFAULT_SEQ')"
+    override fun longAutoincType(): String = "LONG DEFAULT nextval('$DEFAULT_SEQ')"
+
+}
