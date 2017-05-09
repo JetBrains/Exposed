120cdbc3f813171183bb754500b56a5e74e5f985
Ryan McGuinness <rmcguinness@walmartlabs.com>
2017-05-08 18:24:11 -0600

    Switched from the binary key format to String format for more consistent use

--- src/main/kotlin/org/jetbrains/exposed/dao/UUIDEntity.kt
+++ src/main/kotlin/org/jetbrains/exposed/dao/UUIDEntity.kt
@@ -3,10 +3,10 @@ package org.jetbrains.exposed.dao
 import org.jetbrains.exposed.sql.Column
 import java.util.UUID
 
-open class UUIDTable(name: String = "", columName: String = "id") : IdTable<UUID>(name) {
-  override val id: Column<EntityID<UUID>> = uuid(columName).primaryKey().clientDefault { UUID.randomUUID() }.entityId()
+open class UUIDTable(name: String = "", columName: String = "id") : IdTable<String>(name) {
+  override val id: Column<EntityID<String>> = varchar(columName, 36).primaryKey().clientDefault { UUID.randomUUID().toString() }.entityId()
 }
 
-abstract class UUIDEntity(id: EntityID<UUID>) : Entity<UUID>(id)
+abstract class UUIDEntity(id: EntityID<String>) : Entity<String>(id)
 
-abstract class UUIDEntityClass<out E: UUIDEntity>(table: IdTable<UUID>, entityType: Class<E>? = null) : EntityClass<UUID, E> (table)
+abstract class UUIDEntityClass<out E: UUIDEntity>(table: IdTable<String>, entityType: Class<E>? = null) : EntityClass<String, E> (table)
