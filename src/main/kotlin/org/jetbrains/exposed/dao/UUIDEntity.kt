9092a7cc00b85e3fc77ee40f4ce95a91b88954df
Ryan McGuinness <rmcguinness@walmartlabs.com>
2017-05-08 19:21:15 -0600

    fixed unused variable

--- src/main/kotlin/org/jetbrains/exposed/dao/UUIDEntity.kt
+++ src/main/kotlin/org/jetbrains/exposed/dao/UUIDEntity.kt
@@ -4,9 +4,12 @@ import org.jetbrains.exposed.sql.Column
 import java.util.UUID
 
 open class UUIDTable(name: String = "", columName: String = "id") : IdTable<String>(name) {
-  override val id: Column<EntityID<String>> = varchar(columName, 36).primaryKey().clientDefault { UUID.randomUUID().toString().toUpperCase() }.entityId()
+  override val id: Column<EntityID<String>> = varchar(columName, 36)
+      .primaryKey().clientDefault { UUID.randomUUID().toString().toUpperCase() }.entityId()
 }
 
 abstract class UUIDEntity(id: EntityID<String>) : Entity<String>(id)
 
-abstract class UUIDEntityClass<out E: UUIDEntity>(table: IdTable<String>, entityType: Class<E>? = null) : EntityClass<String, E> (table)
+abstract class UUIDEntityClass<out E: UUIDEntity>(table: IdTable<String>,
+                                                  entityType: Class<E>? = null)
+  : EntityClass<String, E> (table, entityType)
