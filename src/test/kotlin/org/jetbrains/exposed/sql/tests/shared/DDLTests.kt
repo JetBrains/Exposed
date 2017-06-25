075df9dcde702dca3baba6b3feacffa65b7b77da
BarakatX2 <barakatx2@gmail.com>
2017-06-25 11:01:33 -0500

    Wrapper method for multi-column unique indices and multi-column index test (#124)
    #124 Wrapper method for multi-column unique indices and multi-column index test 
    * Added wrapper method Table.uniqueIndex for creating unique indices with multiple columns.
    * Made the multi column unique index test more focused on multi column indices in general

--- src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/DDLTests.kt
+++ src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/DDLTests.kt
@@ -184,6 +184,24 @@ class DDLTests : DatabaseTestsBase() {
         }
     }
 
+    @Test fun testMultiColumnIndex() {
+        val t = object : Table("t1") {
+            val type = varchar("type", 255)
+            val name = varchar("name", 255)
+            init {
+                index(false, name, type)
+                uniqueIndex(type, name)
+            }
+        }
+
+        withTables(t) {
+            val indexAlter = SchemaUtils.createIndex(t.indices[0].first, t.indices[0].second)
+            val uniqueAlter = SchemaUtils.createIndex(t.indices[1].first, t.indices[1].second)
+            assertEquals("CREATE INDEX ${"t1_name_type".inProperCase()} ON ${"t1".inProperCase()} (${"name".inProperCase()}, ${"type".inProperCase()})", indexAlter)
+            assertEquals("CREATE UNIQUE INDEX ${"t1_type_name_unique".inProperCase()} ON ${"t1".inProperCase()} (${"type".inProperCase()}, ${"name".inProperCase()})", uniqueAlter)
+        }
+    }
+
     @Test fun testBlob() {
         val t = object: Table("t1") {
             val id = integer("id").autoIncrement("t1_seq").primaryKey()
