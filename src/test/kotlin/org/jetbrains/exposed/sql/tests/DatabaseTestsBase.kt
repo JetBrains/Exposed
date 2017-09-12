fddcf287f95d1af70cd567f7465dc6e58689a903
andrey.tarashevskiy <fantocci@gmail.com>
2017-09-12 21:36:41 +0300

    Run oracle tests within container / Temporary log connection link #4

--- src/test/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
+++ src/test/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
@@ -32,9 +32,7 @@ enum class TestDB(val dialect: DatabaseDialect, val connection: String, val driv
             beforeConnection = { postgresSQLProcess }, afterTestFinished = { postgresSQLProcess.close() }),
     ORACLE(OracleDialect, driver = "oracle.jdbc.OracleDriver", user = "ExposedTest", pass = "12345",
             connection = ("jdbc:oracle:thin:@//${System.getProperty("exposed.test.oracle.host", "192.168.99.100")}" +
-                        ":${System.getProperty("exposed.test.oracle.port", "1521")}/xe.oracle.docker").also {
-                            exposedLogger.info("Oracle connection url: $it")
-                        },
+                        ":${System.getProperty("exposed.test.oracle.port", "1521")}/xe"),
             beforeConnection = {
                 Database.connect(ORACLE.connection, user = "sys as sysdba", password = "oracle", driver = ORACLE.driver)
                 transaction(java.sql.Connection.TRANSACTION_READ_COMMITTED, 1) {
