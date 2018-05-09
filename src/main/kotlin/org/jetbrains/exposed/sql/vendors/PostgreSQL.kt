2331213d36e1a572d645ac72b79676f4bf32ab73
Allan Wang <me@allanwang.ca>
2018-05-09 08:32:21 -0400

    Postgres ignore + replace (#298)
    * Add replace and ignore
    * Test insert ignore
    * fix insert and add back replace
    * Pg replace (#2)
    Make pg replace only use primary keys

--- src/main/kotlin/org/jetbrains/exposed/sql/vendors/PostgreSQL.kt
+++ src/main/kotlin/org/jetbrains/exposed/sql/vendors/PostgreSQL.kt
@@ -29,6 +29,33 @@ internal object PostgreSQLFunctionProvider : FunctionProvider() {
         if (limit != null) transaction.throwUnsupportedException("PostgreSQL doesn't support LIMIT in UPDATE clause.")
         return super.update(targets, columnsAndValues, limit, where, transaction)
     }
+
+    override fun replace(table: Table, data: List<Pair<Column<*>, Any?>>, transaction: Transaction): String {
+
+        val builder = QueryBuilder(true)
+        val sql = if (data.isEmpty()) ""
+        else data.joinToString(prefix = "VALUES (", postfix = ")") { (col, value) ->
+            builder.registerArgument(col, value)
+        }
+
+        val columns = data.map { it.first }
+
+        val def = super.insert(false, table, columns, sql, transaction)
+
+        val uniqueCols = columns.filter { it.indexInPK != null }.sortedBy { it.indexInPK }
+        if (uniqueCols.isEmpty())
+            transaction.throwUnsupportedException("Postgres replace table must supply at least one primary key")
+        val conflictKey = uniqueCols.joinToString { transaction.identity(it) }
+        return def + "ON CONFLICT ($conflictKey) DO UPDATE SET " + columns.joinToString { "${transaction.identity(it)}=EXCLUDED.${transaction.identity(it)}" }
+    }
+
+    override fun insert(ignore: Boolean, table: Table, columns: List<Column<*>>, expr: String, transaction: Transaction): String {
+        val def = super.insert(false, table, columns, expr, transaction)
+        return if (ignore) "$def $onConflictIgnore" else def
+    }
+
+    private const val onConflictIgnore = "ON CONFLICT DO NOTHING"
+
 }
 
 internal class PostgreSQLDialect : VendorDialect(dialectName, PostgreSQLDataTypeProvider, PostgreSQLFunctionProvider) {
