942535de158526147a7c2d5a388c5d110c55430f
Andrey.Tarashevskiy <fantocci@gmail.com>
2019-01-18 13:58:36 +0300

    Mariadb supported and covered with tests / dialect name fixed

--- src/main/kotlin/org/jetbrains/exposed/sql/vendors/MariaDBDialect.kt
+++ src/main/kotlin/org/jetbrains/exposed/sql/vendors/MariaDBDialect.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.exposed.sql.vendors
 
 class MariaDBDialect : MysqlDialect() {
+    override val name: String = dialectName
     companion object {
         const val dialectName = "mariadb"
     }
