a96ac5fa5147d66a413c8797dad70aa4d58698d8
tapanrgohil <tapanrgohil@users.noreply.github.com>
2019-01-18 18:26:40 +0530

    Update SQLiteDialect.kt
    #328
    AUTO_INCREMENT changed to AUTOINCREMENT for sqllite

--- src/main/kotlin/org/jetbrains/exposed/sql/vendors/SQLiteDialect.kt
+++ src/main/kotlin/org/jetbrains/exposed/sql/vendors/SQLiteDialect.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 internal object SQLiteDataTypeProvider : DataTypeProvider() {
-    override fun shortAutoincType(): String = "INTEGER AUTO_INCREMENT"
-    override fun longAutoincType(): String = "INTEGER AUTO_INCREMENT"
+    override fun shortAutoincType(): String = "INTEGER AUTOINCREMENT"
+    override fun longAutoincType(): String = "INTEGER AUTOINCREMENT"
     override fun floatType(): String = "SINGLE"
     override fun booleanToStatementString(bool: Boolean) = if (bool) "1" else "0"
     override fun dateTimeType(): String  = "NUMERIC"
