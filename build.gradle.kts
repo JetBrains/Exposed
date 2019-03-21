ae194419fa2b3bce8bdf7b391c5d1cadb4d07d97
Andrey.Tarashevskiy <fantocci@gmail.com>
2019-03-21 23:51:57 +0300

    Gradle release plugin returned

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     kotlin("jvm") version "1.3.21" apply true
     id("tanvd.kosogor") version "1.0.2" apply true
+    id("net.researchgate.release") version "2.8.0"
 }
 
 subprojects {
     apply(plugin = "tanvd.kosogor")
+    apply(plugin = "net.researchgate.release")
+    afterEvaluate {
+        tasks.getByPath("afterReleaseBuild").dependsOn(tasks.getByPath("bintrayUpload"))
+    }
 }
 
 repositories {
