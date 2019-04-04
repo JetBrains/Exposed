a9378b46d4905fd1d1311840ec45721c0a366eed
Andrey.Tarashevskiy <fantocci@gmail.com>
2019-04-05 00:19:41 +0300

    Removing gradle release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
     kotlin("jvm") version "1.3.21" apply true
     id("tanvd.kosogor") version "1.0.3" apply true
-    id("net.researchgate.release") version "2.8.0"
 }
-
-
-val afterRelease = tasks.getByPath("afterReleaseBuild")
 
 subprojects {
     apply(plugin = "tanvd.kosogor")
