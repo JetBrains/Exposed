a96e1b69039a863bbabc11e7bb03b76948ff5bed
Sergey Zabodalov <sergey.zabodalov@jetbrains.com>
2019-05-06 19:48:42 +0300

    existing index creation fix

--- exposed/src/main/kotlin/org/jetbrains/exposed/sql/vendors/Default.kt
+++ exposed/src/main/kotlin/org/jetbrains/exposed/sql/vendors/Default.kt
@@ -318,11 +318,13 @@ abstract class VendorDialect(override val name: String,
                     }
                 }
                 rs.close()
-                val tColumns = table.columns.associateBy { transaction.identity(it) }
+                val tColumns = table.columns.associateBy { transaction.identity(it).trim('"') }
+
                 tmpIndices.filterNot { it.key.first in pkNames }
                         .mapNotNull { (index, columns) ->
                             columns.mapNotNull { cn -> tColumns[cn] }.takeIf { c -> c.size == columns.size }?.let { c -> Index(c, index.second, index.first) }
                         }
+
             }
         }
         return HashMap(existingIndicesCache)
