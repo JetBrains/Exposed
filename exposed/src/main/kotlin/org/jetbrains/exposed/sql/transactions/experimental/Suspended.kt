3429d514821bea3300d04f9b5aec8d8eed449fd6
Tapac <fantocci@gmail.com>
2019-06-15 00:19:06 +0300

    #553 ThreadLocalTransactionManager issues with executors / Test and deprecations

--- exposed/src/main/kotlin/org/jetbrains/exposed/sql/transactions/experimental/Suspended.kt
+++ exposed/src/main/kotlin/org/jetbrains/exposed/sql/transactions/experimental/Suspended.kt
@@ -3,7 +3,23 @@ package org.jetbrains.exposed.sql.transactions.experimental
 import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.ThreadLocalTransactionManager
 import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.coroutineContext
+
+@Deprecated("Use suspendedTransaction", ReplaceWith("suspendedTransaction"), DeprecationLevel.WARNING)
+suspend fun <T> transaction(db: Database? = null, statement: suspend Transaction.() -> T): T =
+        transaction(TransactionManager.manager.defaultIsolationLevel, TransactionManager.manager.defaultRepetitionAttempts, db, statement)
+
+@Deprecated("Use suspendedTransaction", ReplaceWith("suspendedTransaction"), DeprecationLevel.WARNING)
+suspend fun <T> transaction(transactionIsolation: Int, repetitionAttempts: Int, db: Database? = null, statement: suspend Transaction.() -> T): T =
+        org.jetbrains.exposed.sql.transactions.transaction(transactionIsolation, repetitionAttempts, db) {
+            runBlocking { statement() }
+        }
+
 
 suspend fun <T> suspendedTransaction(context: CoroutineContext? = null, db: Database? = null, statement: Transaction.() -> T): T {
     val threadLocalManager = (db?.let { TransactionManager.managerFor(it) } ?: TransactionManager.manager) as? ThreadLocalTransactionManager
--- exposed/src/test/kotlin/org/jetbrains/exposed/sql/tests/h2/H2Tests.kt
+++ exposed/src/test/kotlin/org/jetbrains/exposed/sql/tests/h2/H2Tests.kt
@@ -1,15 +1,45 @@
 package org.jetbrains.exposed.sql.tests.h2
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.replace
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
 import org.jetbrains.exposed.sql.tests.TestDB
+import org.jetbrains.exposed.sql.transactions.experimental.suspendedTransaction
 import org.junit.Test
 import kotlin.test.assertFailsWith
 
 class H2Tests : DatabaseTestsBase() {
+
+    @Test fun suspendedTx() {
+        withDb(TestDB.H2_MYSQL) {
+            SchemaUtils.create(Testing)
+            runBlocking(Dispatchers.IO) {
+                suspendedTransaction {
+                    Testing.replace {
+                        it[Testing.id] = 1
+                    }
+
+                    launch(Dispatchers.Default) {
+                        suspendedTransaction {
+                            assertEquals(1, Testing.select { Testing.id.eq(1) }.single()[Testing.id])
+                        }
+                    }
+                }
+
+                val result = suspendedTransaction(Dispatchers.Default) {
+                    Testing.select { Testing.id.eq(1) }.single()[Testing.id]
+                }
+
+
+                kotlin.test.assertEquals(1, result)
+            }
+        }
+    }
 
     @Test
     fun replaceInH2WithMySQLMode() {
