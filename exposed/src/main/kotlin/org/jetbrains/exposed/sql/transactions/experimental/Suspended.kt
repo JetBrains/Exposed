bb16dd96ee1777f4c351b24c92fb3afa4ba2b447
Tapac <fantocci@gmail.com>
2019-06-15 00:18:11 +0300

    #553 ThreadLocalTransactionManager issues with executors

--- exposed/src/main/kotlin/org/jetbrains/exposed/sql/transactions/experimental/Suspended.kt
+++ exposed/src/main/kotlin/org/jetbrains/exposed/sql/transactions/experimental/Suspended.kt
@@ -1,14 +1,15 @@
 package org.jetbrains.exposed.sql.transactions.experimental
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
-suspend fun <T> transaction(db: Database? = null, statement: suspend Transaction.() -> T): T =
+suspend fun <T> suspendedTransaction(context: CoroutineContext? = null, db: Database? = null, statement: Transaction.() -> T): T {
+    val threadLocalManager = (db?.let { TransactionManager.managerFor(it) } ?: TransactionManager.manager) as? ThreadLocalTransactionManager
+    val currentContext = context ?: coroutineContext
+    val scope = threadLocalManager?.let { currentContext + it.threadLocal.asContextElement() } ?: currentContext
+    return withContext(scope) {
         transaction(TransactionManager.manager.defaultIsolationLevel, TransactionManager.manager.defaultRepetitionAttempts, db, statement)
-
-suspend fun <T> transaction(transactionIsolation: Int, repetitionAttempts: Int, db: Database? = null, statement: suspend Transaction.() -> T): T =
-    org.jetbrains.exposed.sql.transactions.transaction(transactionIsolation, repetitionAttempts, db) {
-        runBlocking { statement() }
     }
+}
