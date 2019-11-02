9960bd92dccf72cc01f857801bee48dabc96d909
Tapac <fantocci@gmail.com>
2019-11-03 00:17:39 +0900

    Spring dependency updated to 5.2.2 Spring ReactiveTransactionManager tests ignored

--- spring-transaction/build.gradle.kts
+++ spring-transaction/build.gradle.kts
@@ -14,14 +14,14 @@ repositories {
 dependencies {
     api(project(":exposed-core"))
     implementation(project(":exposed-jdbc"))
-    api("org.springframework", "spring-jdbc", "5.1.7.RELEASE")
-    api("org.springframework", "spring-context", "5.1.7.RELEASE")
+    api("org.springframework", "spring-jdbc", "5.2.0.RELEASE")
+    api("org.springframework", "spring-context", "5.2.0.RELEASE")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.0-M1")
     implementation("com.h2database", "h2", "1.4.199")
 
     testImplementation(project(":exposed-dao"))
     testImplementation(kotlin("test-junit"))
-    testImplementation("org.springframework", "spring-test", "5.1.7.RELEASE")
+    testImplementation("org.springframework", "spring-test", "5.2.0.RELEASE")
     testImplementation("org.slf4j", "slf4j-log4j12", "1.7.26")
     testImplementation("log4j", "log4j", "1.2.17")
     testImplementation("junit", "junit", "4.12")
