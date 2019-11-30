43a2810164f08e205aa332a8013e0fa7da95e9db
hichem <hf.fazai@gmail.com>
2019-11-30 19:51:45 +0100

    restore branch

--- exposed-java-time/src/main/kotlin/org/jetbrains/exposed/sql/JavaDateFunctions.kt
+++ exposed-java-time/src/main/kotlin/org/jetbrains/exposed/sql/JavaDateFunctions.kt
@@ -1,14 +1,8 @@
-package org.jetbrains.exposed.sql
+package org.jetbrains.exposed.sql.`java-time`
 
-import org.jetbrains.exposed.exceptions.UnsupportedByDialectException
-import org.jetbrains.exposed.sql.vendors.currentDialect
-import org.jetbrains.exposed.sql.vendors.H2Dialect
-import org.jetbrains.exposed.sql.vendors.MariaDBDialect
-import org.jetbrains.exposed.sql.vendors.MysqlDialect
-import org.jetbrains.exposed.sql.vendors.OracleDialect
-import org.jetbrains.exposed.sql.vendors.PostgreSQLDialect
-import org.jetbrains.exposed.sql.vendors.SQLiteDialect
-import org.jetbrains.exposed.sql.vendors.SQLServerDialect
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Function
+import org.jetbrains.exposed.sql.vendors.*
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.Temporal
@@ -26,104 +20,24 @@ class CurrentDateTime : Function<LocalDateTime>(JavaLocalDateTimeColumnType.INST
     }
 }
 
-class Year<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is SQLServerDialect -> append("YEAR(", expr, ")")
-            is MariaDBDialect -> append("YEAR(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%Y',", expr, ")")
-            is H2Dialect -> append("YEAR(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
 class Month<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
     override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
         when (currentDialect) {
             is PostgreSQLDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
             is OracleDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
+            is OracleDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
             is SQLServerDialect -> append("MONTH(", expr, ")")
             is MariaDBDialect -> append("MONTH(", expr, ")")
             is SQLiteDialect -> append("STRFTIME('%m',", expr, ")")
             is H2Dialect -> append("MONTH(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Day<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is SQLServerDialect -> append("DAY(", expr, ")")
-            is MariaDBDialect -> append("DAY(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%d',", expr, ")")
-            is H2Dialect -> append("DAY(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Hour<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is SQLServerDialect -> append("HOUR(", expr, ")")
-            is MariaDBDialect -> append("HOUR(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%H',", expr, ")")
-            is H2Dialect -> append("HOUR(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Minute<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is SQLServerDialect -> append("MINUTE(", expr, ")")
-            is MariaDBDialect -> append("MINUTE(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%M',", expr, ")")
-            is H2Dialect -> append("MINUTE(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Second<T:Temporal?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is SQLServerDialect -> append("SECOND(", expr, ")")
-            is MariaDBDialect -> append("SECOND(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%S',", expr, ")")
-            is H2Dialect -> append("SECOND(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
+            else -> append("MONTH(", expr, ")")
         }
     }
 }
 
 fun <T: Temporal?> Expression<T>.date() = Date(this)
 
-fun <T: Temporal?> Expression<T>.year() = Year(this)
 fun <T: Temporal?> Expression<T>.month() = Month(this)
-fun <T: Temporal?> Expression<T>.day() = Day(this)
-fun <T: Temporal?> Expression<T>.hour() = Hour(this)
-fun <T: Temporal?> Expression<T>.minute() = Minute(this)
-fun <T: Temporal?> Expression<T>.second() = Second(this)
 
 
 fun dateParam(value: LocalDate): Expression<LocalDate> = QueryParameter(value, JavaLocalDateColumnType.INSTANCE)
--- exposed-jodatime/src/main/kotlin/org/jetbrains/exposed/sql/DateFunctions.kt
+++ exposed-jodatime/src/main/kotlin/org/jetbrains/exposed/sql/DateFunctions.kt
@@ -1,14 +1,8 @@
-package org.jetbrains.exposed.sql
+package org.jetbrains.exposed.sql.jodatime
 
-import org.jetbrains.exposed.exceptions.UnsupportedByDialectException
-import org.jetbrains.exposed.sql.vendors.currentDialect
-import org.jetbrains.exposed.sql.vendors.H2Dialect
-import org.jetbrains.exposed.sql.vendors.MariaDBDialect
-import org.jetbrains.exposed.sql.vendors.MysqlDialect
-import org.jetbrains.exposed.sql.vendors.OracleDialect
-import org.jetbrains.exposed.sql.vendors.PostgreSQLDialect
-import org.jetbrains.exposed.sql.vendors.SQLiteDialect
-import org.jetbrains.exposed.sql.vendors.SQLServerDialect
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Function
+import org.jetbrains.exposed.sql.vendors.*
 import org.joda.time.DateTime
 
 class Date<T:DateTime?>(val expr: Expression<T>): Function<DateTime>(DateColumnType(false)) {
@@ -24,104 +18,23 @@ class CurrentDateTime : Function<DateTime>(DateColumnType(false)) {
     }
 }
 
-class Year<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(YEAR FROM ", expr, ")")
-            is SQLServerDialect -> append("YEAR(", expr, ")")
-            is MariaDBDialect -> append("YEAR(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%Y',", expr, ")")
-            is H2Dialect -> append("YEAR(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
 class Month<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
     override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
         when (currentDialect) {
             is PostgreSQLDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
             is OracleDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(MONTH FROM ", expr, ")")
             is SQLServerDialect -> append("MONTH(", expr, ")")
             is MariaDBDialect -> append("MONTH(", expr, ")")
             is SQLiteDialect -> append("STRFTIME('%m',", expr, ")")
             is H2Dialect -> append("MONTH(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Day<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(DAY FROM ", expr, ")")
-            is SQLServerDialect -> append("DAY(", expr, ")")
-            is MariaDBDialect -> append("DAY(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%d',", expr, ")")
-            is H2Dialect -> append("DAY(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Hour<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(HOUR FROM ", expr, ")")
-            is SQLServerDialect -> append("HOUR(", expr, ")")
-            is MariaDBDialect -> append("HOUR(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%H',", expr, ")")
-            is H2Dialect -> append("HOUR(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Minute<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(MINUTE FROM ", expr, ")")
-            is SQLServerDialect -> append("MINUTE(", expr, ")")
-            is MariaDBDialect -> append("MINUTE(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%M',", expr, ")")
-            is H2Dialect -> append("MINUTE(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
-        }
-    }
-}
-
-class Second<T:DateTime?>(val expr: Expression<T>): Function<Int>(IntegerColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-        when (currentDialect) {
-            is PostgreSQLDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is OracleDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is MysqlDialect -> append("EXTRACT(SECOND FROM ", expr, ")")
-            is SQLServerDialect -> append("SECOND(", expr, ")")
-            is MariaDBDialect -> append("SECOND(", expr, ")")
-            is SQLiteDialect -> append("STRFTIME('%S',", expr, ")")
-            is H2Dialect -> append("SECOND(", expr, ")")
-            else -> throw UnsupportedByDialectException("Unsupported database dialect", currentDialect)
+            else -> append("MONTH(", expr, ")")
         }
     }
 }
 
 fun <T: DateTime?> Expression<T>.date() = Date(this)
 
-fun <T: DateTime?> Expression<T>.year() = Year(this)
 fun <T: DateTime?> Expression<T>.month() = Month(this)
-fun <T: DateTime?> Expression<T>.day() = Day(this)
-fun <T: DateTime?> Expression<T>.hour() = Hour(this)
-fun <T: DateTime?> Expression<T>.minute() = Minute(this)
-fun <T: DateTime?> Expression<T>.second() = Second(this)
 
 
 fun dateParam(value: DateTime): Expression<DateTime> = QueryParameter(value, DateColumnType(false))
