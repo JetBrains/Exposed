12bfd0f6367293e018ae6416e7ed2a62689d0dda
hichem <hf.fazai@gmail.com>
2019-12-02 19:39:58 +0100

    Adding functions tests to jodatime and javatime modules

--- exposed-java-time/src/test/kotlin/org/jetbrains/exposed/JavaTimeTests.kt
+++ exposed-java-time/src/test/kotlin/org/jetbrains/exposed/JavaTimeTests.kt
@@ -1,13 +1,52 @@
 package org.jetbrains.exposed
 
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.`java-time`.*
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
+import org.jetbrains.exposed.sql.tests.TestDB
 import org.jetbrains.exposed.sql.tests.currentDialectTest
+import org.jetbrains.exposed.sql.tests.shared.assertEquals
 import org.jetbrains.exposed.sql.vendors.MysqlDialect
+import org.junit.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.temporal.Temporal
 import kotlin.test.assertEquals
 
+open class JavaTimeBaseTest : DatabaseTestsBase() {
+
+    @Test
+    fun javaTimeFunctions() {
+        withTables(listOf(TestDB.SQLITE), Cities) {
+            SchemaUtils.create(Cities)
+
+            val now = LocalDateTime.now()
+
+            val cityID = Cities.insert {
+                it[name] = "Tunisia"
+                it[local_time] = now
+            } get Cities.id
+
+            val insertedYear = Cities.slice(Cities.local_time.year()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.year()]
+            val insertedMonth = Cities.slice(Cities.local_time.month()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.month()]
+            val insertedDay = Cities.slice(Cities.local_time.day()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.day()]
+            val insertedHour = Cities.slice(Cities.local_time.hour()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.hour()]
+            val insertedMinute = Cities.slice(Cities.local_time.minute()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.minute()]
+            val insertedSecond = Cities.slice(Cities.local_time.second()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.second()]
+
+            assertEquals(now.year, insertedYear)
+            assertEquals(now.month.value, insertedMonth)
+            assertEquals(now.dayOfMonth, insertedDay)
+            assertEquals(now.hour, insertedHour)
+            assertEquals(now.minute, insertedMinute)
+            assertEquals(now.second, insertedSecond)
+        }
+    }
+}
 
 fun <T:Temporal> assertEqualDateTime(d1: T?, d2: T?) {
     when{
@@ -29,3 +68,9 @@ fun equalDateTime(d1: Temporal?, d2: Temporal?) = try {
 }
 
 val today: LocalDate = LocalDate.now()
+
+object Cities : Table() {
+    val id = integer("cityId").autoIncrement("cities_seq").primaryKey() // PKColumn<Int>
+    val name = varchar("name", 50) // Column<String>
+    val local_time = datetime("local_time").nullable() // Column<datetime>
+}
--- exposed-jodatime/src/test/kotlin/org/jetbrains/exposed/JodaTimeTests.kt
+++ exposed-jodatime/src/test/kotlin/org/jetbrains/exposed/JodaTimeTests.kt
@@ -1,15 +1,52 @@
 package org.jetbrains.exposed
 
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.jodatime.*
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
+import org.jetbrains.exposed.sql.tests.TestDB
 import org.jetbrains.exposed.sql.tests.currentDialectTest
+import org.jetbrains.exposed.sql.tests.shared.assertEquals
 import org.jetbrains.exposed.sql.vendors.MysqlDialect
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import org.joda.time.LocalDateTime
+import org.junit.Test
 import kotlin.test.assertEquals
 
 open class JodaTimeBaseTest : DatabaseTestsBase() {
     init {
         DateTimeZone.setDefault(DateTimeZone.UTC)
+    }
+
+    @Test
+    fun jodaTimeFunctions() {
+        withTables(listOf(TestDB.SQLITE), Cities) {
+            SchemaUtils.create(Cities)
+
+            val now = LocalDateTime.now()
+
+            val cityID = Cities.insert {
+                it[name] = "St. Petersburg"
+                it[local_time] = now.toDateTime()
+            } get Cities.id
+
+            val insertedYear = Cities.slice(Cities.local_time.year()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.year()]
+            val insertedMonth = Cities.slice(Cities.local_time.month()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.month()]
+            val insertedDay = Cities.slice(Cities.local_time.day()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.day()]
+            val insertedHour = Cities.slice(Cities.local_time.hour()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.hour()]
+            val insertedMinute = Cities.slice(Cities.local_time.minute()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.minute()]
+            val insertedSecond = Cities.slice(Cities.local_time.second()).select { Cities.id.eq(cityID) }.single()[Cities.local_time.second()]
+
+            assertEquals(now.year, insertedYear)
+            assertEquals(now.monthOfYear, insertedMonth)
+            assertEquals(now.dayOfMonth, insertedDay)
+            assertEquals(now.hourOfDay, insertedHour)
+            assertEquals(now.minuteOfHour, insertedMinute)
+            assertEquals(now.secondOfMinute, insertedSecond)
+        }
     }
 }
 
@@ -34,3 +71,9 @@ fun equalDateTime(d1: DateTime?, d2: DateTime?) = try {
 }
 
 val today: DateTime = DateTime.now().withTimeAtStartOfDay()
+
+object Cities : Table() {
+    val id = integer("cityId").autoIncrement("cities_seq").primaryKey() // PKColumn<Int>
+    val name = varchar("name", 50) // Column<String>
+    val local_time = datetime("local_time").nullable() // Column<datetime>
+}
