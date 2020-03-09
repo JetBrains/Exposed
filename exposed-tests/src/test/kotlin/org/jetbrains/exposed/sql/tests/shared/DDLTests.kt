713649c3d1073eedec608e26ab7c2192ab17f2ed
hichem <hf.fazai@gmail.com>
2020-03-09 02:46:45 +0100

    test enhancement

--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/DDLTests.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/DDLTests.kt
@@ -14,6 +14,7 @@ import org.jetbrains.exposed.sql.tests.currentDialectTest
 import org.jetbrains.exposed.sql.tests.inProperCase
 import org.jetbrains.exposed.sql.tests.shared.dml.DMLTestsData
 import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.vendors.PostgreSQLDialect
 import org.jetbrains.exposed.sql.vendors.SQLiteDialect
 import org.junit.Test
 import org.postgresql.util.PGobject
@@ -794,17 +795,24 @@ class DDLTests : DatabaseTestsBase() {
     @Test
     fun createTableWithForeignKeyToAnotherSchema() {
         withDb(excludeSettings = listOf(TestDB.SQLITE)) {
-            exec("CREATE SCHEMA ${"one".inProperCase()}")
-            exec("CREATE SCHEMA ${"two".inProperCase()}")
-            SchemaUtils.create(TableFromSchemeOne, TableFromSchemeTwo)
-            val idFromOne = TableFromSchemeOne.insertAndGetId {  }
+            try {
+                exec("CREATE SCHEMA ${"one".inProperCase()}")
+                exec("CREATE SCHEMA ${"two".inProperCase()}")
+                SchemaUtils.create(TableFromSchemeOne, TableFromSchemeTwo)
+                val idFromOne = TableFromSchemeOne.insertAndGetId { }
 
-            TableFromSchemeTwo.insert {
-                it[reference] = idFromOne
+                TableFromSchemeTwo.insert {
+                    it[reference] = idFromOne
+                }
+
+                assertEquals(1, TableFromSchemeOne.selectAll().count())
+                assertEquals(1, TableFromSchemeTwo.selectAll().count())
+            } finally {
+                if(currentDialectTest is PostgreSQLDialect) {
+                    exec("DROP SCHEMA IF EXISTS ${"one".inProperCase()} CASCADE")
+                    exec("DROP SCHEMA IF EXISTS ${"two".inProperCase()} CASCADE")
+                }
             }
-
-            assertEquals(1, TableFromSchemeOne.selectAll().count())
-            assertEquals(1, TableFromSchemeTwo.selectAll().count())
         }
     }
 
