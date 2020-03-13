8f4e8d55a1e508f90535f670339a221739157e00
hichem <hf.fazai@gmail.com>
2020-03-13 01:11:52 +0100

    test fix

--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/SchemaTests.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/SchemaTests.kt
@@ -5,13 +5,14 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
 import org.jetbrains.exposed.sql.tests.TestDB
 import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.vendors.PostgreSQLNGDialect
 import org.jetbrains.exposed.sql.vendors.SQLServerDialect
 import org.jetbrains.exposed.sql.vendors.currentDialect
 import org.junit.Test
 
 class SchemaTests : DatabaseTestsBase() {
     @Test
-    fun schemaTests()  {
+    fun `create and set schema in mysql`() {
         withDb(TestDB.MYSQL) {
             val schema = Schema("MYSCHEMA")
             try {
@@ -21,16 +22,19 @@ class SchemaTests : DatabaseTestsBase() {
                 val catalogName = connection.catalog
 
                 assertEquals(catalogName, schema.identifier)
-            }
-            finally {
+            } finally {
                 SchemaUtils.dropSchema(schema)
             }
         }
+    }
 
+    @Test
+    fun `create and set schema tests`() {
         // MARIADB also uses catalogs in data manipulation
-        withDb (excludeSettings = listOf(TestDB.MYSQL, TestDB.MARIADB)) {
-            if(currentDialect.supportsCreateSchema) {
-                val schema = if(currentDialect is SQLServerDialect) {
+        withDb(excludeSettings = listOf(TestDB.MYSQL, TestDB.MARIADB)) {
+            currentDialect
+            if (currentDialect.supportsCreateSchema) {
+                val schema = if (currentDialect is SQLServerDialect) {
                     exec("GRANT CREATE SCHEMA TO guest")
                     exec("SETUSER 'guest'")
                     Schema("MYSCHEMA", "guest")
@@ -42,17 +46,27 @@ class SchemaTests : DatabaseTestsBase() {
                     SchemaUtils.createSchema(schema)
                     SchemaUtils.setSchema(schema)
 
-                    val schemaName = connection.schema
+                    /** connection.schema in Pstgresql-ng is always null in current pgjdbc-ng version (0.8.3).
+                     * This is fixed in pgjdbc-ng repo but not yet released. So here we retrieve the current schema using sql query
+                     * rather than connection.schema */
+                    val schemaName = if(currentDialect is PostgreSQLNGDialect) {
+                         exec("SELECT current_schema()") { rs ->
+                            if (rs.next()) { rs.getString(1) } else { "" }
+                        }
+                    } else {
+                        connection.schema
+                    }
 
                     assertEquals(TransactionManager.current().db.identifierManager.inProperCase(schema.identifier), schemaName)
-                }
-                finally {
+                } finally {
                     SchemaUtils.dropSchema(schema)
                 }
             }
         }
+    }
 
-        // Test references between databases in MySQL
+    @Test
+    fun `table references table with same name in other database in mysql`() {
         withDb(TestDB.MYSQL) {
             val schema = Schema("MYSCHEMA")
             try {
@@ -67,8 +81,7 @@ class SchemaTests : DatabaseTestsBase() {
                 val catalogName = connection.catalog
 
                 assertEquals(catalogName, schema.identifier)
-            }
-            finally {
+            } finally {
                 SchemaUtils.dropSchema(schema)
             }
         }
