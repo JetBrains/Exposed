67ca25a8cfb973e8c7bc5f616df42be384e3536d
Andrey.Tarashevskiy <fantocci@gmail.com>
2020-05-19 16:17:02 +0300

    ExposedBlob.equals/hashCode

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/statements/api/ExposedBlob.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/statements/api/ExposedBlob.kt
@@ -1,3 +1,14 @@
 package org.jetbrains.exposed.sql.statements.api
 
-class ExposedBlob(val bytes: ByteArray)
+class ExposedBlob(val bytes: ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ExposedBlob) return false
+
+        if (!bytes.contentEquals(other.bytes)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = bytes.contentHashCode()
+}
