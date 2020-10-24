4ebb38cd2a90fef129424c1c9095e2ea8558e0d9
Andrey.Tarashevskiy <fantocci@gmail.com>
2020-10-25 00:56:54 +0300

    Build exposed-java-time with Kotlin 1.4 as a target

--- exposed-java-time/build.gradle.kts
+++ exposed-java-time/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
 tasks.withType<KotlinJvmCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
-        apiVersion = "1.3"
-        languageVersion = "1.3"
+        apiVersion = "1.4"
+        languageVersion = "1.4"
     }
 }
 
