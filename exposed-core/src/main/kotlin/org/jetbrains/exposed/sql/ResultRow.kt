19049bd16d49813273b8e9fcda42a0980676334c
hichem <hf.fazai@gmail.com>
2020-12-11 00:34:11 +0100

    getRaw index is based on indexOf expression

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/ResultRow.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/ResultRow.kt
@@ -55,9 +55,12 @@ class ResultRow(val fieldIndex: Map<Expression<*>, Int>) {
             return c.restoreValueFromParts(rawParts)
         }
 
-        val index = fieldIndex[c]
-            ?: ((c as? Column<*>)?.columnType as? EntityIDColumnType<*>)?.let { fieldIndex[it.idColumn] }
-            ?: error("$c is not in record set")
+        val index = if (fieldIndex.keys.indexOf(c) >= 0) {
+            fieldIndex.keys.indexOf(c)
+        } else {
+            ((c as? Column<*>)?.columnType as? EntityIDColumnType<*>)?.let { fieldIndex[it.idColumn] }
+                    ?: error("$c is not in record set")
+        }
 
         return data[index] as T?
     }
