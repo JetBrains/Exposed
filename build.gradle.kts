f818784a05b8b5f814114b9276d1073dfdb38f34
Joel Feinstein <jnfeinstein@gmail.com>
2021-01-13 21:36:59 -0500

    Add linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     kotlin("jvm") version "1.4.21" apply true
+    id("org.jmailen.kotlinter") version "3.3.0"
     id("tanvd.kosogor") version "1.0.9" apply true
+}
+
+allprojects {
+    apply(plugin = "org.jmailen.kotlinter")
 }
 
 subprojects {
