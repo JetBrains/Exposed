447e4c784406bef3e089a274d54746189ca7de42
hfazai <hf.fazai@gmail.com>
2021-01-26 15:39:28 +0100

    like and notLike mothods works with string expression

---
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/ddl/WhereConditionsTests.kt
@@ -0,0 +1,46 @@
+package org.jetbrains.exposed.sql.tests.shared.ddl
+
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.stringLiteral
+import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
+import org.junit.Test
+
+class WhereConditionsTests: DatabaseTestsBase() {
+  object User: Table() {
+    val name = varchar("name", 20)
+  }
+
+  @Test
+  fun whereLikeExpressionTest() {
+    withTables(User) {
+      User.insert {
+        it[name] = "Hichem"
+      }
+      val namesResult = User.select {
+        User.name like stringLiteral("Hich%")
+      }.map { it[User.name] }
+
+      assertEquals(1, namesResult.size)
+      assertEquals("Hichem", namesResult.first())
+    }
+  }
+
+  @Test
+  fun whereNotLikeExpressionTest() {
+    withTables(User) {
+      User.insert {
+        it[name] = "Hichem"
+      }
+      val namesResult = User.select {
+        User.name notLike stringLiteral("Hich%")
+      }.map { it }
+
+      assertTrue(namesResult.isEmpty())
+    }
+  }
+}
