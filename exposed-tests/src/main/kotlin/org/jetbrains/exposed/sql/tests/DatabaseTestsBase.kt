35f9b639195a246f5e8a5cb357ad84fc3adce707
hfazai <hf.fazai@gmail.com>
2021-02-14 21:07:58 +0100

    Add nullCatalogMeansCurrent to connection properties for mysql5.1

--- exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
+++ exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
@@ -26,7 +26,7 @@ enum class TestDB(val connection: () -> String, val driver: String, val user: St
                 val host = System.getProperty("exposed.test.mysql.host") ?: System.getProperty("exposed.test.mysql8.host")
                 val port = System.getProperty("exposed.test.mysql.port") ?: System.getProperty("exposed.test.mysql8.port")
                 host.let { dockerHost ->
-                    "jdbc:mysql://$dockerHost:$port/testdb?useSSL=false&characterEncoding=UTF-8"
+                    "jdbc:mysql://$dockerHost:$port/testdb?useSSL=false&characterEncoding=UTF-8?nullCatalogMeansCurrent=false"
                 }
             }
         },
