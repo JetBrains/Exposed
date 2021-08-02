978707e4191d2615a364b4b90843b1b2dffe73c0
Viktor Alenkov <viktor@alenkov.name>
2021-08-02 15:40:03 +0300

    * Corrected under comments * Updated Detekt settings

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 
 dependencies {
     gradleApi()
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.5.21")
+    implementation("org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.jvm.gradle.plugin", "1.5.21")
     implementation("com.avast.gradle", "gradle-docker-compose-plugin", "0.14.2")
     implementation("io.github.gradle-nexus", "publish-plugin", "1.0.0")
     implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.17.1")
