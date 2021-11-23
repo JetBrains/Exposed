40a4bae2cfbd268f8fc5f1bfada625230babe1fb
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2021-11-23 15:53:52 -0500

    c

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/Queries.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/Queries.kt
@@ -218,17 +218,28 @@ fun <T : Table> T.insertIgnore(
 /**
  * @sample org.jetbrains.exposed.sql.tests.shared.DMLTests.testUpdate01
  */
-fun <T : Table> T.update(where: (SqlExpressionBuilder.() -> Op<Boolean>)? = null, limit: Int? = null, body: T.(UpdateStatement) -> Unit): Int {
-    val query = UpdateStatement(this, limit, where?.let { SqlExpressionBuilder.it() })
-    body(query)
-    return query.execute(TransactionManager.current())!!
-}
+fun <T : Table> T.update(where: (SqlExpressionBuilder.() -> Op<Boolean>)? = null,
+                         limit: Int? = null,
+                         body: T.(UpdateStatement) -> Unit) : Int = (materializeDefaultScope()
+    ?.let { defaultScope ->
+        where?.let { defaultScope and SqlExpressionBuilder.it() } ?: defaultScope
+    } ?: where?.let { SqlExpressionBuilder.it() }).let { reducedOp ->
+        UpdateStatement(this, limit, reducedOp)
+            .also { body(it) }
+            .execute(TransactionManager.current())!!
+    }
 
-fun Join.update(where: (SqlExpressionBuilder.() -> Op<Boolean>)? = null, limit: Int? = null, body: (UpdateStatement) -> Unit): Int {
-    val query = UpdateStatement(this, limit, where?.let { SqlExpressionBuilder.it() })
-    body(query)
-    return query.execute(TransactionManager.current())!!
-}
+fun Join.update(where: (SqlExpressionBuilder.() -> Op<Boolean>)? = null,
+                limit: Int? = null,
+                body: (UpdateStatement) -> Unit) : Int = (
+    materializeDefaultScope()?.let { defaultScope ->
+        where?.let { defaultScope and SqlExpressionBuilder.it() } ?: defaultScope
+    } ?: where?.let { SqlExpressionBuilder.it() })
+    .let { reducedWhere ->
+        UpdateStatement(this, limit, reducedWhere)
+            .also(body)
+            .execute(TransactionManager.current())!!
+    }
 
 /**
  * @sample org.jetbrains.exposed.sql.tests.shared.DDLTests.tableExists02
--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/dml/UpdateTests.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/dml/UpdateTests.kt
@@ -3,14 +3,18 @@ package org.jetbrains.exposed.sql.tests.shared.dml
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.exceptions.UnsupportedByDialectException
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
 import org.jetbrains.exposed.sql.tests.TestDB
+import org.jetbrains.exposed.sql.tests.shared.assertEqualLists
 import org.jetbrains.exposed.sql.tests.shared.assertEquals
+import org.jetbrains.exposed.sql.tests.shared.entities.EntityTests
 import org.jetbrains.exposed.sql.tests.shared.expectException
-import org.jetbrains.exposed.sql.vendors.SQLiteDialect
+import org.jetbrains.exposed.sql.vendors.*
 import org.junit.Test
 import java.lang.IllegalArgumentException
-import java.lang.IllegalStateException
+import kotlin.test.assertNotEquals
+
 
 class UpdateTests : DatabaseTestsBase() {
     private val notSupportLimit by lazy {
@@ -21,26 +25,73 @@ class UpdateTests : DatabaseTestsBase() {
         exclude
     }
 
+    private val unscopedScopedUsers = object : Table(DMLTestsData.ScopedUsers.tableName) {
+        val id: Column<String> = varchar("id", 10)
+        val name: Column<String> = varchar("name", length = 50)
+        val cityId: Column<Int?> = reference("city_id", DMLTestsData.Cities.id).nullable()
+        val flags: Column<Int> = integer("flags").default(0)
+        override val primaryKey = PrimaryKey(id)
+    }
+
+    private val unscopedScopedUserData = object : Table(DMLTestsData.ScopedUserData.tableName) {
+        val userId: Column<String> = reference("user_id", DMLTestsData.ScopedUsers.id)
+        val comment: Column<String> = varchar("comment", 30)
+        val value: Column<Int> = integer("value")
+    }
+
+
+
     @Test
     fun testUpdate01() {
-        withCitiesAndUsers { _, users, _, _, _ ->
+        withCitiesAndUsers { _, users, _, scopedUsers, _ ->
             val alexId = "alex"
-            val alexName = users.slice(users.name).select { users.id.eq(alexId) }.first()[users.name]
-            assertEquals("Alex", alexName)
+            users.slice(users.name)
+                .select { users.id.eq(alexId) }
+                .first()[users.name]
+                .let { alexName -> assertEquals("Alex", alexName) }
 
-            val newName = "Alexey"
+            val newAlexName = "Alexey"
             users.update({ users.id.eq(alexId) }) {
-                it[users.name] = newName
+                it[users.name] = newAlexName
             }
 
-            val alexNewName = users.slice(users.name).select { users.id.eq(alexId) }.first()[users.name]
-            assertEquals(newName, alexNewName)
+            users.slice(users.name)
+                .select { users.id.eq(alexId) }
+                .first()[users.name]
+                .let { actualNewAlexName -> assertEquals(newAlexName, actualNewAlexName) }
+
+            val sergeyId = "sergey"
+            unscopedScopedUsers.slice(unscopedScopedUsers.name)
+                .select { unscopedScopedUsers.id inList listOf(alexId, sergeyId) }
+                .orderBy(unscopedScopedUsers.name, SortOrder.ASC)
+                .map { it[unscopedScopedUsers.name] }
+                .let { names -> assertEqualLists(names, "Alex", "Sergey") }
+
+            scopedUsers.update({ scopedUsers.id.eq(alexId) }) {
+                it[scopedUsers.name] = newAlexName
+            }
+            unscopedScopedUsers.slice(unscopedScopedUsers.name)
+                .select { unscopedScopedUsers.id inList listOf(alexId, sergeyId) }
+                .orderBy(unscopedScopedUsers.name, SortOrder.ASC)
+                .map { it[unscopedScopedUsers.name] }
+                .let { names -> assertEqualLists(names, "Alex", "Sergey") }
+
+            val newSergeyName = "Aye! I'm Sergey!"
+            scopedUsers.update({ scopedUsers.id.eq(sergeyId) }) {
+                it[scopedUsers.name] = newSergeyName
+            }
+            unscopedScopedUsers.slice(unscopedScopedUsers.name)
+                .select { unscopedScopedUsers.id inList listOf(alexId, sergeyId) }
+                .orderBy(unscopedScopedUsers.name, SortOrder.ASC)
+                .map { it[unscopedScopedUsers.name] }
+                .let { names -> assertEqualLists(names, "Alex", newSergeyName) }
+
         }
     }
 
     @Test
     fun testUpdateWithLimit01() {
-        withCitiesAndUsers(exclude = notSupportLimit) { _, users, _, _, _ ->
+        withCitiesAndUsers(exclude = notSupportLimit) { _, users, _, scopedUsers, _ ->
             val aNames = users.slice(users.name).select { users.id like "a%" }.map { it[users.name] }
             assertEquals(2, aNames.size)
 
@@ -52,6 +103,30 @@ class UpdateTests : DatabaseTestsBase() {
             val changed = users.slice(users.name).select { users.id eq "NewName" }.count()
             assertEquals(1, unchanged)
             assertEquals(1, changed)
+
+            scopedUsers.slice(scopedUsers.name)
+                .select { scopedUsers.cityId eq munichId() }
+                .map { it[scopedUsers.name] }
+                .let { munichUsers ->
+                    assertEquals(2, munichUsers.size)
+
+                    scopedUsers.update(
+                        { scopedUsers.cityId eq munichId() },
+                        1
+                    ) { it[users.name] = "NewName" }
+
+                    val unchanged = scopedUsers.slice(scopedUsers.name)
+                        .select {
+                            (scopedUsers.cityId eq munichId())
+                            .and(scopedUsers.name neq "NewName")
+                        }.count()
+
+                    val changed = scopedUsers.slice(scopedUsers.name)
+                        .select { scopedUsers.name eq "NewName" }
+                        .count()
+                    assertEquals(1, unchanged)
+                    assertEquals(1, changed)
+                }
         }
     }
 
@@ -70,16 +145,49 @@ class UpdateTests : DatabaseTestsBase() {
     @Test
     fun testUpdateWithJoin() {
         val dialects = listOf(TestDB.SQLITE)
-        withCitiesAndUsers(dialects) { cities, users, userData, _, _ ->
-            val join = users.innerJoin(userData)
-            join.update {
-                it[userData.comment] = users.name
-                it[userData.value] = 123
-            }
+        withCitiesAndUsers(dialects) { _, users, userData, scopedUsers, scopedUserData ->
+            users.innerJoin(userData)
+                .let { join ->
+                    join.update {
+                        it[userData.comment] = users.name
+                        it[userData.value] = 123
+                    }
 
-            join.selectAll().forEach {
-                assertEquals(it[users.name], it[userData.comment])
-                assertEquals(123, it[userData.value])
+                    join.selectAll().forEach {
+                        assertEquals(it[users.name], it[userData.comment])
+                        assertEquals(123, it[userData.value])
+                    }
+                }
+
+            if (currentDialect is PostgreSQLDialect ||
+                currentDialect is PostgreSQLNGDialect ||
+                currentDialect is OracleDialect ||
+                currentDialect is SQLServerDialect ||
+                currentDialect is MysqlDialect ||
+                currentDialect is SQLiteDialect) {
+                scopedUsers.innerJoin(scopedUserData)
+                    .let { join ->
+                        // Only Sergey should be affected by this update.
+                        join.update {
+                            it[scopedUserData.comment] = scopedUsers.name
+                            it[scopedUserData.value] = 123
+                        }.let { assertEquals(1, it) }
+
+                        join.selectAll().toList().let { rows ->
+                            assertEquals(1, rows.size)
+                            rows.first().let { row ->
+                                assertEquals(row[scopedUsers.name], row[scopedUserData.comment])
+                                assertEquals(123, row[scopedUserData.value])
+                            }
+                        }
+
+                        unscopedScopedUsers.innerJoin(unscopedScopedUserData)
+                            .select { unscopedScopedUsers.id neq "sergey" }
+                            .forEach { row ->
+                                assertNotEquals(row[scopedUsers.name], row[scopedUserData.comment])
+                                assertNotEquals(123, row[scopedUserData.value])
+                            }
+                    }
             }
         }
     }
