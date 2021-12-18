bc8a2a86823808568080b1ed472867d9e33b8d5e
naftalmm <naftalmm@gmail.com>
2021-12-19 01:58:53 +0300

    Minor refactoring

--- exposed-dao/build.gradle.kts
+++ exposed-dao/build.gradle.kts
@@ -1,7 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.exposed.gradle.setupDialectTest
-
 plugins {
     kotlin("jvm") apply true
 }
