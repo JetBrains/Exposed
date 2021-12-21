5503140be4fb70663ee5254ad027940aff2a13e3
naftalmm <naftalmm@gmail.com>
2021-12-21 13:15:38 +0300

    Don't run related composeUp (and composeDown) tasks if DBTestWithDockerCompose task is UP-TO-DATE/SKIPPED

--- buildSrc/src/main/kotlin/org/jetbrains/exposed/gradle/tasks/DBTestWithDockerCompose.kt
+++ buildSrc/src/main/kotlin/org/jetbrains/exposed/gradle/tasks/DBTestWithDockerCompose.kt
@@ -1,10 +1,7 @@
 package org.jetbrains.exposed.gradle.tasks
 
 import com.avast.gradle.dockercompose.ComposeSettings
-import org.gradle.api.Task
 import org.gradle.api.tasks.Input
-import org.gradle.process.JavaForkOptions
-import org.gradle.process.ProcessForkOptions
 import org.jetbrains.exposed.gradle._dockerCompose
 import java.io.File
 import java.time.Duration
@@ -19,39 +16,32 @@ open class DBTestWithDockerCompose(dialect: String, @get:Input val port: Int, @g
     @Inject
     constructor(parameters: Parameters) : this(parameters.dialect, parameters.port, parameters.dockerComposeServiceName)
 
-    private val dockerCompose: ComposeSettings = project._dockerCompose.nested(dockerComposeServiceName)
-
-    init {
-        dockerCompose.apply {
-            isRequiredWithTaskConfigurationAvoidanceBy(this@DBTestWithDockerCompose)
-            environment.put("COMPOSE_CONVERT_WINDOWS_PATHS", true)
-            useComposeFiles.add(
-                File(project.rootProject.projectDir, "buildScripts/docker/docker-compose-$dockerComposeServiceName.yml").absolutePath
-            )
-            captureContainersOutput.set(true)
-            removeVolumes.set(true)
-            waitForHealthyStateTimeout.set(Duration.ofMinutes(60))
-        }
+    private val dockerCompose: ComposeSettings = project._dockerCompose.nested(dockerComposeServiceName).apply {
+        environment.put("COMPOSE_CONVERT_WINDOWS_PATHS", true)
+        useComposeFiles.add(
+            File(project.rootProject.projectDir, "buildScripts/docker/docker-compose-$dockerComposeServiceName.yml").absolutePath
+        )
+        captureContainersOutput.set(true)
+        removeVolumes.set(true)
+        waitForHealthyStateTimeout.set(Duration.ofMinutes(60))
     }
 
     override fun executeTests() {
-        val containerInfo = dockerCompose.servicesInfos[dockerComposeServiceName]!!
-        withSystemProperties(
-            "exposed.test.$dockerComposeServiceName.host" to containerInfo.host,
-            "exposed.test.$dockerComposeServiceName.port" to (containerInfo.ports[port] ?: -1)
-        ) {
-            super.executeTests()
+        with(dockerCompose) {
+            try {
+                upTask.get().up()
+                exposeAsEnvironment(this@DBTestWithDockerCompose)
+                exposeAsSystemProperties(this@DBTestWithDockerCompose)
+                val containerInfo = servicesInfos[dockerComposeServiceName]!!
+                withSystemProperties(
+                    "exposed.test.$dockerComposeServiceName.host" to containerInfo.host,
+                    "exposed.test.$dockerComposeServiceName.port" to (containerInfo.ports[port] ?: -1)
+                ) {
+                    super.executeTests()
+                }
+            } finally {
+                downTask.get().down()
+            }
         }
     }
-}
-
-fun ComposeSettings.isRequiredWithTaskConfigurationAvoidanceBy(task: Task) {
-    task.dependsOn(upTask)
-    task.finalizedBy(downTask)
-    upTask.configure {
-        shouldRunAfter(project.tasks.named("testClasses"))
-    }
-
-    if (task is ProcessForkOptions) task.doFirst { exposeAsEnvironment(task) }
-    if (task is JavaForkOptions) task.doFirst { exposeAsSystemProperties(task) }
 }
