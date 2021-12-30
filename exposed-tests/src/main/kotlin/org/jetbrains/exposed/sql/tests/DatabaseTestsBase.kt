2b3edab2ba0dbe16950d3ccd2ca6ec1aa1243f1a
naftalmm <naftalmm@gmail.com>
2021-12-30 23:42:15 +0300

    fix detekt.LongParameterList warnings

--- exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
+++ exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/DatabaseTestsBase.kt
@@ -22,7 +22,6 @@ enum class TestDB(
     val pass: String = "",
     val beforeConnection: () -> Unit = {},
     val afterTestFinished: () -> Unit = {},
-    var db: Database? = null,
     val dbConfig: DatabaseConfig.Builder.() -> Unit = {}
 ) {
     H2({ "jdbc:h2:mem:regular;DB_CLOSE_DELAY=-1;" }, "org.h2.Driver", dbConfig = {
@@ -103,6 +102,8 @@ enum class TestDB(
         },
         "org.mariadb.jdbc.Driver"
     );
+
+    var db: Database? = null
 
     fun connect(configure: DatabaseConfig.Builder.() -> Unit = {}): Database {
         val config = DatabaseConfig {
