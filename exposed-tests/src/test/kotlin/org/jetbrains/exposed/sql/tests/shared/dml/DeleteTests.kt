d70c2f160c8e72b6a8224976e9d261e91ef77b26
Alexey Soshin <alexey.soshin@gmail.com>
2022-08-14 16:37:31 +0100

    Go back to `deleteWhere`, since `deleteIgnoreWhere` is not supported by most dialects

--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/dml/DeleteTests.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/dml/DeleteTests.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.exposed.sql.tests.shared.dml
 
+import junit.framework.TestCase.assertNull
 import org.jetbrains.exposed.exceptions.UnsupportedByDialectException
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
@@ -41,8 +42,18 @@ class DeleteTests : DatabaseTestsBase() {
     @Test
     fun testDeleteTableInContext() {
         withCitiesAndUsers { _, users, userData ->
+            userData.deleteAll()
+            val userDataExists = userData.selectAll().any()
+            assertEquals(false, userDataExists)
+
+            val smthId = users.slice(users.id).select { users.name.like("%thing") }.single()[users.id]
+            assertEquals("smth", smthId)
+
             // Now deleteWhere and deleteIgnoreWhere should bring the table it operates on into context
-            users.deleteIgnoreWhere { name like "%thing" }
+            users.deleteWhere { name like "%thing" }
+
+            val hasSmth = users.select { users.name.like("%thing") }.firstOrNull()
+            assertNull(hasSmth)
         }
     }
 
