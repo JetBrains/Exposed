2d2f1a415e24943eb222b6d8b77112ae41181543
Jocelyne <jocelyne.abihaidar@jetbrains.com>
2023-07-12 12:08:09 +0200

    chore: Remove MaximumLineLength detekt issues

--- exposed-dao/src/main/kotlin/org/jetbrains/exposed/dao/EntityClass.kt
+++ exposed-dao/src/main/kotlin/org/jetbrains/exposed/dao/EntityClass.kt
@@ -479,7 +479,12 @@ abstract class EntityClass<ID : Comparable<ID>, out T : Entity<ID>>(
      * Can be useful when references target the same entities. That will prevent from loading them multiple times (per each reference row) and will require
      * less memory/bandwidth for "heavy" entities (with a lot of columns or columns with huge data in it)
      */
-    fun <SID : Comparable<SID>> warmUpLinkedReferences(references: List<EntityID<SID>>, linkTable: Table, forUpdate: Boolean? = null, optimizedLoad: Boolean = false): List<T> {
+    fun <SID : Comparable<SID>> warmUpLinkedReferences(
+        references: List<EntityID<SID>>,
+        linkTable: Table,
+        forUpdate: Boolean? = null,
+        optimizedLoad: Boolean = false
+    ): List<T> {
         if (references.isEmpty()) return emptyList()
 
         val sourceRefColumn = linkTable.columns.singleOrNull { it.referee == references.first().table.id } as? Column<EntityID<SID>>
--- exposed-java-time/src/test/kotlin/org/jetbrains/exposed/MiscTableTest.kt
+++ exposed-java-time/src/test/kotlin/org/jetbrains/exposed/MiscTableTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MaximumLineLength")
+
 package org.jetbrains.exposed
 
 import org.jetbrains.exposed.sql.*
@@ -1240,7 +1242,7 @@ class MiscTableTest : DatabaseTestsBase() {
                 exec("INSERT IGNORE INTO `zerodatetimetable` (dt1,dt2,ts1,ts2) VALUES ('0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');")
                 val row = ZeroDateTimeTable.selectAll().first()
 
-                for (c in listOf(ZeroDateTimeTable.dt1, ZeroDateTimeTable.dt2, ZeroDateTimeTable.ts1, ZeroDateTimeTable.ts2)) {
+                listOf(ZeroDateTimeTable.dt1, ZeroDateTimeTable.dt2, ZeroDateTimeTable.ts1, ZeroDateTimeTable.ts2).forEach { c ->
                     val actual = row[c]
                     assertNull(actual, "$c expected null but was $actual")
                 }
