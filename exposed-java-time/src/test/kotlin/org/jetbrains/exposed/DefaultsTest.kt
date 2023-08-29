dcf23550374c10ef53467ea5e1795442cfb6ee90
ymotchi <allzumenschliches000-service@yahoo.co.jp>
2023-08-29 23:44:32 +0900

    fix: EXPOSED-158 insert space before WHEN
    added the test code
    and removed unnecessary code for this problem

--- exposed-java-time/src/test/kotlin/org/jetbrains/exposed/DefaultsTest.kt
+++ exposed-java-time/src/test/kotlin/org/jetbrains/exposed/DefaultsTest.kt
@@ -26,7 +26,6 @@ import org.jetbrains.exposed.sql.vendors.SQLServerDialect
 import org.jetbrains.exposed.sql.vendors.h2Mode
 import org.junit.Test
 import java.time.*
-import java.time.temporal.ChronoUnit
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -401,7 +400,7 @@ class DefaultsTest : DatabaseTestsBase() {
         java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone(ZoneOffset.UTC))
         assertEquals("UTC", ZoneId.systemDefault().id)
 
-        val nowWithTimeZone = OffsetDateTime.now().truncatedTo(ChronoUnit.MICROS)
+        val nowWithTimeZone = OffsetDateTime.now()
         val timestampWithTimeZoneLiteral = timestampWithTimeZoneLiteral(nowWithTimeZone)
 
         val testTable = object : IntIdTable("t") {
