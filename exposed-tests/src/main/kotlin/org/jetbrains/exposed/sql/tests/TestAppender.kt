62d284c55468960ca5faca5c787456a6ed60b998
Jocelyne <jocelyne.abihaidar@jetbrains.com>
2023-08-31 14:10:57 +0200

    fix: Verbose logging introduced by LogCaptor
    Add TestAppender programmatically for one test rather than enable it for all tests.

--- exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/TestAppender.kt
+++ exposed-tests/src/main/kotlin/org/jetbrains/exposed/sql/tests/TestAppender.kt
@@ -1,24 +1,32 @@
 package org.jetbrains.exposed.sql.tests
 
-import org.apache.logging.log4j.core.Filter
+import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.core.Layout
 import org.apache.logging.log4j.core.LogEvent
 import org.apache.logging.log4j.core.appender.AbstractAppender
 import org.apache.logging.log4j.core.config.Property
 import org.apache.logging.log4j.core.config.plugins.Plugin
-import org.apache.logging.log4j.core.config.plugins.PluginAttribute
-import org.apache.logging.log4j.core.config.plugins.PluginElement
 import org.apache.logging.log4j.core.config.plugins.PluginFactory
 import java.io.Serializable
 
+/**
+ * Appends log events of a specified level to a list using a layout.
+ */
 @Plugin(name = "TestAppender", category = "Core", elementType = "appender", printObject = false)
 class TestAppender
-private constructor(name: String, filter: Filter?, layout: Layout<out Serializable>) :
-    AbstractAppender(name, filter, layout, true, Property.EMPTY_ARRAY) {
+private constructor(layout: Layout<out Serializable>, val level: Level) : AbstractAppender(
+    "TestAppender",
+    null,
+    layout,
+    true,
+    Property.EMPTY_ARRAY
+) {
     private val log: MutableList<LogEvent> = mutableListOf()
 
     override fun append(event: LogEvent) {
-        log.add(event.toImmutable())
+        if (event.level == level) { // add events of the specified level only
+            log.add(event.toImmutable())
+        }
     }
 
     fun getLog(): List<LogEvent> {
@@ -29,11 +37,10 @@ private constructor(name: String, filter: Filter?, layout: Layout<out Serializab
         @PluginFactory
         @JvmStatic
         fun createAppender(
-            @PluginAttribute("name") name: String,
-            @PluginElement("Layout") layout: Layout<out Serializable>,
-            @PluginElement("Filter") filter: Filter?
+            layout: Layout<out Serializable>,
+            level: Level
         ): TestAppender {
-            return TestAppender(name, filter, layout)
+            return TestAppender(layout, level)
         }
     }
 }
