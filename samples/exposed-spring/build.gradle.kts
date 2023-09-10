4d9f311d433160d40c80267faa88f71ce82bef38
육진혁 (Ivan) <ivan@daangn.com>
2023-09-10 14:51:50 +0900

    fix: fit kotlin version to other ktor samples

--- samples/exposed-spring/build.gradle.kts
+++ samples/exposed-spring/build.gradle.kts
@@ -5,8 +5,8 @@ val exposedVersion: String by project
 plugins {
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
 }
 
 group = "org.jetbrains.exposed"
