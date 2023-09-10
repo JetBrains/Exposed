bf1a3a5a7dc652e14e696b0feb0d30092c0b5b94
육진혁 (Ivan) <ivan@daangn.com>
2023-09-10 14:52:59 +0900

    fix: remove needless empty line

--- samples/exposed-spring/src/main/kotlin/controller/UserController.kt
+++ samples/exposed-spring/src/main/kotlin/controller/UserController.kt
@@ -16,13 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
     private val userService: UserService,
 ) {
-
     // Read User
     @GetMapping("/{id}")
     fun findUserById(
