10e663010f0ada58c90f8f8778fcd94e83e724fc
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2023-12-19 15:17:23 -0500

    fix: EXPOSED-242 [PostgreSQL] Cannot change connection setting in middle of a transaction
    Adjust Spring connection spy tests as readOnly no longer set on every connection.

--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/h2/ConnectionPoolTests.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/h2/ConnectionPoolTests.kt
@@ -9,7 +9,8 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.tests.LogDbInTestName
 import org.jetbrains.exposed.sql.tests.TestDB
 import org.jetbrains.exposed.sql.tests.shared.assertEquals
