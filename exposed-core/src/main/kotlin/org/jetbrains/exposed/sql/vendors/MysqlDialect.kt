4808971344c3f965758481b545c906aef2580255
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2024-04-16 12:31:53 -0400

    fix: EXPOSED-340 Syntax error using upsert with MySQL8 below 8.0.19
    MySQL `upsert()` uses an alias for the insert row values when the database
    version is 8+.
    Technically this is only possible starting with version 8.0.19.
    
    Existing function `MysqlDialect.isMysql8` calls `isVersionCovers()`, which only
    fetches the database's major and minor version, so it is not sufficient for
    this comparison.
    To get the patch version as well requires fetching metadata using the full
    `databaseProductVersion` property.

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/vendors/MysqlDialect.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/vendors/MysqlDialect.kt
@@ -243,7 +243,7 @@ internal open class MysqlFunctionProvider : FunctionProvider() {
         }
 
         val isAliasSupported = when (val dialect = transaction.db.dialect) {
-            is MysqlDialect -> dialect !is MariaDBDialect && dialect.isMysql8
+            is MysqlDialect -> dialect !is MariaDBDialect && transaction.db.isFullVersionCovered("8.0.19")
             else -> false // H2_MySQL mode also uses this function provider & requires older version
         }
 
@@ -269,6 +269,13 @@ internal open class MysqlFunctionProvider : FunctionProvider() {
             }
             toString()
         }
+    }
+
+    /** Whether the major, minor, & patch version of the database is equal to or greater than the provided [version]. */
+    private fun Database.isFullVersionCovered(version: String): Boolean {
+        val fullVersion = metadata { databaseProductVersion }.split('.').map(String::toInt)
+        val otherVersion = version.split('.').map(String::toInt)
+        return fullVersion.zip(otherVersion).all { (full, other) -> full >= other }
     }
 }
 
