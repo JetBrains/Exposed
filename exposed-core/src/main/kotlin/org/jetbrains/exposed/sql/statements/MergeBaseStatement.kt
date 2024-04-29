9784a1b11d7b172cf0819881c321518534ebbf5f
Oleg Babichev <oleg.babichev@jetbrains.com>
2024-04-29 13:26:16 +0200

    update KDoc

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/statements/MergeBaseStatement.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/statements/MergeBaseStatement.kt
@@ -32,16 +32,38 @@ abstract class MergeBaseStatement(val table: Table) : Statement<Int>(
         return result
     }
 
+    /**
+     * Defines an insert operation to be performed when there is no matching record in the destination table.
+     *
+     * @param and An additional optional condition [Op<Boolean>] to refine when the insert should take place.
+     * @param body A lambda to configure the [InsertStatement] in which the required columns and corresponding
+     * values are specified for the insert operation.
+     */
     fun insertWhenNotMatched(and: Op<Boolean>? = null, body: (InsertStatement<Int>) -> Unit) {
         val insert = InsertStatement<Int>(originalTable).apply(body)
         whenBranches.add(WhenBranchData(insert.arguments!!.first(), MergeWhenAction.INSERT, and))
     }
 
+    /**
+     * Defines an update operation to be executed when a matching record is found in the destination table.
+     *
+     * @param and An additional optional condition [Op<Boolean>] to specify when the update should occur.
+     * @param deleteWhere Applicable only to Oracle SQL. A condition [Op<Boolean>] used to indicate condition of row deletion.
+     * Oracle SQL does not support separate "delete" clause.
+     * @param body A lambda to define the [UpdateStatement] which sets the values of columns in the matching
+     * records.
+     */
     fun updateWhenMatched(and: Op<Boolean>? = null, deleteWhere: Op<Boolean>? = null, body: (UpdateStatement) -> Unit) {
         val update = UpdateStatement(originalTable, limit = 1).apply(body)
         whenBranches.add(WhenBranchData(update.firstDataSet, MergeWhenAction.UPDATE, and, deleteWhere))
     }
 
+    /**
+     * Specifies a delete operation to be executed when a matching record is found in the destination table.
+     *
+     * @param and An additional optional condition [Op<Boolean>] to determine when the delete operation
+     * should be performed.
+     */
     fun deleteWhenMatched(and: Op<Boolean>? = null) {
         whenBranches.add(WhenBranchData(emptyList(), MergeWhenAction.DELETE, and))
     }
