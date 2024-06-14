23684bd70ac5236fc44171a1aed3bda3798433c9
Pavel Yazev <pavel.yazev@katemedia.eu>
2024-06-14 21:22:22 +0300

    Transaction.withDataBaseLock to report the lock acquiring result

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/SchemaUtils.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/SchemaUtils.kt
@@ -923,8 +923,10 @@ object SchemaUtils {
      *
      * All code provided in _body_ closure will be executed only if there is no another code which running under "withDataBaseLock" at same time.
      * That means that concurrent execution of long running tasks under "database lock" might lead to that only first of them will be really executed.
+     *
+     * @return true - if the code was executed, false - otherwise
      */
-    fun <T> Transaction.withDataBaseLock(body: () -> T) {
+    fun <T> Transaction.withDataBaseLock(body: () -> T): Boolean {
         val buzyTable = object : Table("busy") {
             val busy = bool("busy").uniqueIndex()
         }
@@ -939,6 +941,8 @@ object SchemaUtils {
                 connection.commit()
             }
         }
+
+        return !isBusy
     }
 
     /**
