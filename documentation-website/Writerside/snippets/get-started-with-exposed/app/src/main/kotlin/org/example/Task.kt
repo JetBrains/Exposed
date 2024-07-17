08e6c09ea6b9109a4fff416048dcbab0fa193d28
Viktoriya Nikolova <contact@viknikolova.com>
2024-07-17 16:09:29 +0200

    replace the "Get started with Exposed" tutorial with new content and add sample project

---
+++ documentation-website/Writerside/snippets/get-started-with-exposed/app/src/main/kotlin/org/example/Task.kt
@@ -0,0 +1,8 @@
+import org.jetbrains.exposed.sql.Table
+
+object Tasks : Table() {
+    val id = integer("id").autoIncrement()
+    val title  = varchar("name", 128)
+    val description  = varchar("description", 128)
+    val isCompleted = bool("completed").default(false)
+}
