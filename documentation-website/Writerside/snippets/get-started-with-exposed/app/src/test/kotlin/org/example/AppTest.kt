20397c35764f11f5df616bc2e82cf5297260a8db
Viktoriya Nikolova <contact@viknikolova.com>
2024-07-23 16:20:09 +0200

    remove test function and .gitignore file

--- documentation-website/Writerside/snippets/get-started-with-exposed/app/src/test/kotlin/org/example/AppTest.kt
+++ documentation-website/Writerside/snippets/get-started-with-exposed/app/src/test/kotlin/org/example/AppTest.kt
@@ -7,8 +7,4 @@ import kotlin.test.Test
 import kotlin.test.assertNotNull
 
 class AppTest {
-    @Test fun appHasAGreeting() {
-//        val classUnderTest = App()
-//        assertNotNull(classUnderTest.greeting, "app should have a greeting")
-    }
 }
