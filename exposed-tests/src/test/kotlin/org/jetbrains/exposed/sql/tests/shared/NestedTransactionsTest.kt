84dab61f22d4f35a31c22164702bd4397c5351b5
bog-walk <82039410+bog-walk@users.noreply.github.com>
2024-08-04 10:10:56 -0400

    fix: EXPOSED-439 Outer transaction commits rows from failed inner transaction (#2186)
    * fix: EXPOSED-439 Outer transaction commits rows from caught inner transaction
    
    If a nested transaction fails and is caught in a try-catch block, the exception
    will be caught but any operations will still go through the commit phase. This
    happens because exceptions are only being caught by the outer transaction block>
    Replicating the try-catch logic from inTopLevelTransaction solves the issue.

--- exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/NestedTransactionsTest.kt
+++ exposed-tests/src/test/kotlin/org/jetbrains/exposed/sql/tests/shared/NestedTransactionsTest.kt
@@ -1,14 +1,25 @@
 package org.jetbrains.exposed.sql.tests.shared
 
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.DatabaseConfig
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.deleteAll
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.tests.DatabaseTestsBase
+import org.jetbrains.exposed.sql.tests.TestDB
 import org.jetbrains.exposed.sql.tests.shared.dml.DMLTestsData
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.inTopLevelTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.transactionManager
+import org.junit.Assume
 import org.junit.Test
+import java.sql.SQLException
+import kotlin.test.assertContains
+import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
+import kotlin.test.fail
 
 class NestedTransactionsTest : DatabaseTestsBase() {
 
@@ -68,6 +79,77 @@ class NestedTransactionsTest : DatabaseTestsBase() {
             }
 
             assertNotNull(TransactionManager.currentOrNull())
+        }
+    }
+
+    @Test
+    fun testNestedTransactionNotCommittedAfterFailure() {
+        Assume.assumeTrue(TestDB.H2_V2 in TestDB.enabledDialects())
+        val db = Database.connect(
+            "jdbc:h2:mem:db1;DB_CLOSE_DELAY=-1;", "org.h2.Driver", "root", "",
+            databaseConfig = DatabaseConfig {
+                useNestedTransactions = true
+                defaultMaxAttempts = 1
+            }
+        )
+
+        fun assertSingleRecordInNewTransactionAndReset() = transaction(db) {
+            val result = DMLTestsData.Cities.selectAll().single()[DMLTestsData.Cities.name]
+            assertEquals("City A", result)
+            DMLTestsData.Cities.deleteAll()
+        }
+        val fakeSQLString = "BROKEN_SQL_THAT_CAUSES_EXCEPTION"
+
+        transaction(db) {
+            SchemaUtils.create(DMLTestsData.Cities)
+        }
+
+        transaction(db) {
+            val outerTxId = this.id
+
+            DMLTestsData.Cities.insert { it[name] = "City A" }
+            assertEquals(1, DMLTestsData.Cities.selectAll().count())
+
+            try {
+                inTopLevelTransaction(db.transactionManager.defaultIsolationLevel, db = db) {
+                    val innerTxId = this.id
+                    assertNotEquals(outerTxId, innerTxId)
+
+                    DMLTestsData.Cities.insert { it[name] = "City B" }
+                    exec("$fakeSQLString()")
+                }
+                fail("Should have thrown an exception")
+            } catch (cause: SQLException) {
+                assertContains(cause.toString(), fakeSQLString)
+            }
+        }
+
+        assertSingleRecordInNewTransactionAndReset()
+
+        transaction(db) {
+            val outerTxId = this.id
+
+            DMLTestsData.Cities.insert { it[name] = "City A" }
+            assertEquals(1, DMLTestsData.Cities.selectAll().count())
+
+            try {
+                transaction(db) {
+                    val innerTxId = this.id
+                    assertNotEquals(outerTxId, innerTxId)
+
+                    DMLTestsData.Cities.insert { it[name] = "City B" }
+                    exec("$fakeSQLString()")
+                }
+                fail("Should have thrown an exception")
+            } catch (cause: SQLException) {
+                assertContains(cause.toString(), fakeSQLString)
+            }
+        }
+
+        assertSingleRecordInNewTransactionAndReset()
+
+        transaction(db) {
+            SchemaUtils.drop(DMLTestsData.Cities)
         }
     }
 }
