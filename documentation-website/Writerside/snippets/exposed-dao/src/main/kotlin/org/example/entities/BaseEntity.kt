95712943cc3550e4018342d725b42371808be845
bog-walk <82039410+bog-walk@users.noreply.github.com>
2024-10-17 15:46:18 -0400

    docs: EXPOSED-572 Add code sample for 'auto-fill columns on entity change﻿' (#2278)
    * docs: EXPOSED-572 Add code sample for 'auto-fill columns on entity change﻿'
    
    Replace link to external sample with refactored code:
    - Place code in new dao snippets subdirectory
    - Add dependency to snippets on kotlin-datetime module
    - Refactor original sample to ensure that update doesn't trigger continuous updates
    
    * docs: EXPOSED-572 Add code sample for 'auto-fill columns on entity change﻿'
    
    - Edit section content to use second person
    - Break down sample sections more descriptively & sequentially
    - Add missing API links

---
+++ documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/BaseEntity.kt
@@ -0,0 +1,11 @@
+package org.example.entities
+
+import kotlinx.datetime.LocalDateTime
+import org.example.tables.BaseTable
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.id.EntityID
+
+abstract class BaseEntity(id: EntityID<Int>, table: BaseTable) : IntEntity(id) {
+    val created: LocalDateTime by table.created
+    var modified: LocalDateTime? by table.modified
+}
---
+++ documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/BaseEntityClass.kt
@@ -0,0 +1,43 @@
+package org.example.entities
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.example.tables.BaseTable
+import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.exposedLogger
+
+abstract class BaseEntityClass<out E : BaseEntity>(
+    table: BaseTable
+) : IntEntityClass<E>(table) {
+    init {
+        EntityHook.subscribe { change ->
+            val changedEntity = change.toEntity(this)
+            when (val type = change.changeType) {
+                EntityChangeType.Updated -> {
+                    val now = nowUTC()
+                    changedEntity?.let {
+                        if (it.writeValues[table.modified as Column<Any?>] == null) {
+                            it.modified = now
+                        }
+                    }
+                    logChange(changedEntity, type, now)
+                }
+                else -> logChange(changedEntity, type)
+            }
+        }
+    }
+
+    private fun nowUTC() = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+
+    private fun logChange(entity: E?, type: EntityChangeType, dateTime: LocalDateTime? = null) {
+        entity?.let {
+            val entityClassName = this::class.java.enclosingClass.simpleName
+            exposedLogger.info(
+                "$entityClassName(${it.id}) ${type.name.lowercase()} at ${dateTime ?: nowUTC()}"
+            )
+        }
+    }
+}
