90b54ca8c5c3598c2799eb7d0937c021a34a15a4
Viktoriya Nikolova <contact@viknikolova.com>
2024-11-11 18:53:25 +0100

    fix examples issues

--- documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/CityEntity.kt
+++ documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/CityEntity.kt
@@ -1,12 +1,12 @@
 package org.example.entities
 
 import org.example.tables.CitiesTable
+import org.jetbrains.exposed.dao.ImmutableEntityClass
 import org.jetbrains.exposed.dao.IntEntity
-import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
-class City(id: EntityID<Int>) : IntEntity(id) {
-    val name by Countries.name
+class CityEntity(id: EntityID<Int>) : IntEntity(id) {
+    val name by CitiesTable.name
 
     companion object : ImmutableEntityClass<City>(CitiesTable)
 }
--- documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/UserEntityWithOverride.kt
+++ documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/UserEntityWithOverride.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
-class User(id: EntityID<Int>) : IntEntity(id) {
+class UserEntityWithOverride(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<User>(UsersTable)
 
     var name by UsersTable.name
