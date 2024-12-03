56cdde39dbd876e78608d00cfbf56d86191c4551
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2024-12-03 18:48:43 -0500

    chore: Fix docs/snippets exposed-dao project build fail
    EntityWithBase64.kt file was causing compile errors due to unresolved objects
    that did not have import statements.

--- documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/EntityWithBase64.kt
+++ documentation-website/Writerside/snippets/exposed-dao/src/main/kotlin/org/example/entities/EntityWithBase64.kt
@@ -4,12 +4,15 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 object TableWithText : IntIdTable() {
     val text = varchar("text", length = 2048)
 }
 
 class EntityWithBase64(id: EntityID<Int>) : IntEntity(id) {
+    @OptIn(ExperimentalEncodingApi::class)
     var base64: String by TableWithText.text
         .memoizedTransform(
             wrap = { Base64.encode(it.toByteArray()) },
