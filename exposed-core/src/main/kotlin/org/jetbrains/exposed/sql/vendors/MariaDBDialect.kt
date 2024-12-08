d9be5f8dd2f12a1c3024520de62960b2f1b11121
devgor88 <devgordevgor@mail.ru>
2024-12-08 14:15:35 +0300

    fix: sequences table implemented in 11.5 version

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/vendors/MariaDBDialect.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/sql/vendors/MariaDBDialect.kt
@@ -87,7 +87,7 @@ class MariaDBDialect : MysqlDialect() {
     }
 
     companion object : DialectNameProvider("MariaDB") {
-        const val SEQUENCE_MIN_MAJOR_VERSION = 10
-        const val SEQUENCE_MIN_MINOR_VERSION = 3
+        const val SEQUENCE_MIN_MAJOR_VERSION = 11
+        const val SEQUENCE_MIN_MINOR_VERSION = 5
     }
 }
