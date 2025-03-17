a94c7a31da0ca78d760572987d4a001a77b23b3c
Oleg Babichev <oleg.babichev@jetbrains.com>
2025-03-17 10:25:40 +0100

    Fix Merge statement (same as Update/Delete statements)

--- exposed-r2dbc/src/main/kotlin/org/jetbrains/exposed/r2dbc/sql/statements/MergeSuspendExecutable.kt
+++ exposed-r2dbc/src/main/kotlin/org/jetbrains/exposed/r2dbc/sql/statements/MergeSuspendExecutable.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.exposed.r2dbc.sql.statements
 
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.jetbrains.exposed.r2dbc.sql.R2dbcTransaction
 import org.jetbrains.exposed.r2dbc.sql.statements.api.R2dbcPreparedStatementApi
 import org.jetbrains.exposed.sql.statements.MergeStatement
@@ -8,6 +9,7 @@ open class MergeSuspendExecutable<S : MergeStatement>(
     override val statement: S
 ) : SuspendExecutable<Int, S> {
     override suspend fun R2dbcPreparedStatementApi.executeInternal(transaction: R2dbcTransaction): Int? {
-        return executeUpdate()
+        executeUpdate()
+        return this.getResultRow()?.rowsUpdated()?.awaitFirstOrNull()?.toInt() ?: 0
     }
 }
