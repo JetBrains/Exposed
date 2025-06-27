3a7e6ca78d562ca9542b5dec6f036a66fbadf048
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2025-06-26 21:48:24 -0400

    refactor: Address multiple metadata TODOs

--- exposed-r2dbc/src/main/kotlin/org/jetbrains/exposed/v1/r2dbc/vendors/PostgreSQLDialectMetadata.kt
+++ exposed-r2dbc/src/main/kotlin/org/jetbrains/exposed/v1/r2dbc/vendors/PostgreSQLDialectMetadata.kt
@@ -6,6 +6,3 @@ package org.jetbrains.exposed.v1.r2dbc.vendors
 open class PostgreSQLDialectMetadata : DatabaseDialectMetadata() {
     override suspend fun supportsLimitWithUpdateOrDelete(): Boolean = false
 }
-
-// TODO clean up PostgresqlNG - if not supported, why here & in R2dbcDatabase.registerDialect()???
-class PostgreSQLNGDialectMetadata : PostgreSQLDialectMetadata()
