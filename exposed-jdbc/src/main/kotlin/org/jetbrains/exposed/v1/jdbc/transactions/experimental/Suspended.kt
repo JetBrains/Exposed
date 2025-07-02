f847e1b0933e6f8e40395399d60e67fad2674689
Oleg Babichev <oleg.babichev@jetbrains.com>
2025-07-02 09:12:51 +0200

    fix: add TODO about wrapping exception on attempts limit

--- exposed-jdbc/src/main/kotlin/org/jetbrains/exposed/v1/jdbc/transactions/experimental/Suspended.kt
+++ exposed-jdbc/src/main/kotlin/org/jetbrains/exposed/v1/jdbc/transactions/experimental/Suspended.kt
@@ -214,6 +214,7 @@ private fun <T> TransactionScope.suspendedTransactionAsyncInternal(
                 handleSQLException(cause, transaction, attempts)
                 attempts++
                 if (attempts >= transaction.maxAttempts) {
+                    // TODO Probably we should wrap the exception to indicate attempts
                     throw cause
                 }
 
