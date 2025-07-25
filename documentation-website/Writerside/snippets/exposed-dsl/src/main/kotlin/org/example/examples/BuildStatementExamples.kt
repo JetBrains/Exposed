d4817e12f47933e5afc28393e18d8f78c511fcd5
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2025-07-25 00:43:04 -0400

    docs: Add section on DSL buildStatement() and prepareSQL()
    - Add review suggestion
    - Add new example file in exposed-dsl snippets project
    - Replace hard-coded examples with reference to new file

---
+++ documentation-website/Writerside/snippets/exposed-dsl/src/main/kotlin/org/example/examples/BuildStatementExamples.kt
@@ -0,0 +1,66 @@
+package org.example.examples
+
+import org.example.tables.StarWarsFilmsTable
+import org.jetbrains.exposed.v1.core.Transaction
+import org.jetbrains.exposed.v1.core.statements.buildStatement
+import org.jetbrains.exposed.v1.jdbc.*
+
+/*
+    Important: The contents of this file are referenced by line number in `DSL-Statement-Builder.md`.
+    If you add, remove, or modify any lines, ensure you update the corresponding
+    line numbers in the `code-block` element of the referenced file.
+*/
+
+class BuildStatementExamples {
+    fun Transaction.prepareQuerySql() {
+        val filmQuery = StarWarsFilmsTable
+            .selectAll()
+            .where { StarWarsFilmsTable.sequelId lessEq 3 }
+
+        val querySql: String = filmQuery
+            .orWhere { StarWarsFilmsTable.sequelId greater 6 }
+            .prepareSQL(this)
+        println(querySql)
+
+        /*
+            SELECT STARWARSFILMS.ID, STARWARSFILMS.SEQUEL_ID, STARWARSFILMS."name", STARWARSFILMS.DIRECTOR
+            FROM STARWARSFILMS
+            WHERE (STARWARSFILMS.SEQUEL_ID <= ?) OR (STARWARSFILMS.SEQUEL_ID > ?)
+        */
+
+        val fullQuerySql = filmQuery
+            .orWhere { StarWarsFilmsTable.sequelId greater 6 }
+            .prepareSQL(this, prepared = false)
+        println(fullQuerySql)
+
+        /*
+            SELECT STARWARSFILMS.ID, STARWARSFILMS.SEQUEL_ID, STARWARSFILMS."name", STARWARSFILMS.DIRECTOR
+            FROM STARWARSFILMS
+            WHERE (STARWARSFILMS.SEQUEL_ID <= 3) OR (STARWARSFILMS.SEQUEL_ID > 6)
+         */
+    }
+
+    fun Transaction.prepareInsertSql() {
+        val insertFilm = buildStatement {
+            StarWarsFilmsTable.insert {
+                it[sequelId] = 8
+                it[name] = "The Last Jedi"
+                it[director] = "Rian Johnson"
+            }
+        }
+
+        val preparedSql: String = insertFilm.prepareSQL(this, prepared = true)
+        println(preparedSql)
+
+        /*
+            INSERT INTO STARWARSFILMS (SEQUEL_ID, "name", DIRECTOR) VALUES (?, ?, ?)
+         */
+
+        val fullSql: String = insertFilm.prepareSQL(this, prepared = false)
+        println(fullSql)
+
+        /*
+            INSERT INTO STARWARSFILMS (SEQUEL_ID, "name", DIRECTOR) VALUES (8, 'The Last Jedi', 'Rian Johnson')
+         */
+    }
+}
