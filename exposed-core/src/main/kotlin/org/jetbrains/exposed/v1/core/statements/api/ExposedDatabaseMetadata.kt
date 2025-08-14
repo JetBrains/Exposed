0396aa1b5b71852cbbed9fde279054005b5d2402
bog-walk <82039410+bog-walk@users.noreply.github.com>
2025-08-13 22:00:03 -0400

    chore: Remove more obsolete TODOs (#2589)
    These are TODOs that wer eeither found to no longer be valid, or were agreed to
    be deferred to post-V1 (due to them being internal refactorings).

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/v1/core/statements/api/ExposedDatabaseMetadata.kt
+++ exposed-core/src/main/kotlin/org/jetbrains/exposed/v1/core/statements/api/ExposedDatabaseMetadata.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.exposed.v1.core.statements.api
 
-import org.jetbrains.exposed.v1.core.InternalApi
 import org.jetbrains.exposed.v1.core.ReferenceOption
 
 /**
@@ -11,14 +10,11 @@ abstract class ExposedDatabaseMetadata(val database: String) {
     /** Clears and resets any stored information about the database's current schema to default values. */
     abstract fun resetCurrentScheme()
 
-    @Suppress("ForbiddenComment")
-    // TODO: THIS should become protected after the usage in DatabaseDialect is fully deprecated
     /**
      * Returns the corresponding [ReferenceOption] for the specified [refOption] result,
      * or `null` if the database result is an invalid string without a corresponding match.
      */
-    @InternalApi
-    abstract fun resolveReferenceOption(refOption: String): ReferenceOption?
+    protected abstract fun resolveReferenceOption(refOption: String): ReferenceOption?
 
     /** Clears any cached values. */
     abstract fun cleanCache()
