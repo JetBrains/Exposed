b67f06e11ce9661c410ac5d7969676ffb68c93af
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2026-02-05 20:24:45 -0500

    feat: Rename Spring reactive transaction module & rebase

---
+++ spring7-reactive-transaction/src/test/kotlin/org/jetbrains/exposed/v1/spring7/reactive/transaction/ConnectionFactorySpy.kt
@@ -0,0 +1,31 @@
+package org.jetbrains.exposed.v1.spring7.reactive.transaction
+
+import io.r2dbc.spi.Connection
+import io.r2dbc.spi.ConnectionFactories
+import io.r2dbc.spi.ConnectionFactory
+import io.r2dbc.spi.ConnectionFactoryMetadata
+import kotlinx.coroutines.reactive.awaitFirst
+import org.reactivestreams.Publisher
+import org.springframework.r2dbc.connection.ConnectionFactoryUtils
+import reactor.core.publisher.Mono
+
+class ConnectionFactorySpy(
+    private val connectionSpy: (Connection) -> Connection
+) : ConnectionFactory {
+    private val connectionPublisher = ConnectionFactoryUtils.getConnection(
+        ConnectionFactories.get("r2dbc:h2:mem:///test")
+    )
+
+    private lateinit var con: Connection
+
+    suspend fun getCon(): Connection {
+        if (::con.isInitialized.not()) {
+            con = connectionSpy(connectionPublisher.awaitFirst())
+        }
+        return con
+    }
+
+    override fun create(): Publisher<out Connection?> = Mono.just(con)
+
+    override fun getMetadata(): ConnectionFactoryMetadata = throw NotImplementedError()
+}
