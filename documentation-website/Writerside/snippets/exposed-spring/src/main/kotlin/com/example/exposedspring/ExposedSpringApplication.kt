fc34fa2f7ce20c8e8de425221f7dd1bd4168d008
vnikolova <contact@viknikolova.com>
2026-02-17 18:20:52 +0100

    docs: fix: detekt issues

--- documentation-website/Writerside/snippets/exposed-spring/src/main/kotlin/com/example/exposedspring/ExposedSpringApplication.kt
+++ documentation-website/Writerside/snippets/exposed-spring/src/main/kotlin/com/example/exposedspring/ExposedSpringApplication.kt
@@ -3,15 +3,16 @@ package com.example.exposedspring
 import org.jetbrains.exposed.v1.spring.boot4.autoconfigure.ExposedAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@ImportAutoConfiguration(value = [ExposedAutoConfiguration::class],
+@ImportAutoConfiguration(
+    value = [ExposedAutoConfiguration::class],
     exclude = [DataSourceTransactionManagerAutoConfiguration::class]
 )
 class ExposedSpringApplication
 
-fun main(args: Array<String>) {
-    runApplication<ExposedSpringApplication>(*args)
+fun main() {
+    runApplication<ExposedSpringApplication>()
 }
