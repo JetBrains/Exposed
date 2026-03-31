a71f82deddca2fac67392544674b9948ffa21e48
bog-walk <82039410+bog-walk@users.noreply.github.com>
2026-03-31 07:45:20 -0400

    fix: EXPOSED-994 JSONB contains() produces invalid SQL when JSON array contains single quote (#2768)

--- exposed-r2dbc-tests/src/test/kotlin/org/jetbrains/exposed/v1/r2dbc/sql/tests/json/JsonBColumnTests.kt
+++ exposed-r2dbc-tests/src/test/kotlin/org/jetbrains/exposed/v1/r2dbc/sql/tests/json/JsonBColumnTests.kt
@@ -9,6 +9,7 @@ import kotlinx.serialization.builtins.ArraySerializer
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.v1.core.*
+import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.core.dao.id.IntIdTable
 import org.jetbrains.exposed.v1.core.vendors.PostgreSQLDialect
 import org.jetbrains.exposed.v1.exceptions.UnsupportedByDialectException
@@ -23,6 +24,7 @@ import org.jetbrains.exposed.v1.r2dbc.tests.R2dbcDatabaseTestsBase
 import org.jetbrains.exposed.v1.r2dbc.tests.TestDB
 import org.jetbrains.exposed.v1.r2dbc.tests.currentDialectTest
 import org.jetbrains.exposed.v1.r2dbc.tests.shared.assertEqualCollections
+import org.jetbrains.exposed.v1.r2dbc.tests.shared.assertEqualLists
 import org.jetbrains.exposed.v1.r2dbc.tests.shared.assertEquals
 import org.jetbrains.exposed.v1.r2dbc.tests.shared.assertFalse
 import org.jetbrains.exposed.v1.r2dbc.tests.shared.assertTrue
@@ -237,6 +239,44 @@ class JsonBColumnTests : R2dbcDatabaseTestsBase() {
 
             val hasAtLeast3Numbers = tester.numbers.exists("[2]", optional = optional)
             assertEquals(tripleId, tester.selectAll().where { hasAtLeast3Numbers }.single()[tester.id])
+        }
+    }
+
+    @Test
+    fun testJsonBContainsWithIterables() {
+        val iterables = object : IntIdTable("iterables") {
+            val userList = jsonb<List<User>>("user_list", Json.Default)
+            val userSet = jsonb<Set<User>>("user_set", Json.Default)
+            val userArray = jsonb<Array<User>>("user_array", Json.Default)
+            val stringList = jsonb<List<String>>("string_list", Json.Default)
+        }
+
+        suspend fun selectIdWhere(condition: () -> Op<Boolean>): List<EntityID<Int>> {
+            val query = iterables.select(iterables.id).where(condition())
+            return query.map { it[iterables.id] }.toList()
+        }
+
+        withTables(excludeSettings = binaryJsonNotSupportedDB + TestDB.ALL_H2_V2, iterables) {
+            val user1 = User("A", "Team A")
+            val user2 = User("B", "Team B")
+            val stringsWithApostrophes = listOf("They're", "O'", "Connor's")
+            val id1 = iterables.insertAndGetId {
+                it[userList] = listOf(user1, user2)
+                it[userSet] = setOf(user1)
+                it[userArray] = arrayOf(user1, user2)
+                it[stringList] = listOf("Kotlin", "Java")
+            }
+            val id2 = iterables.insertAndGetId {
+                it[userList] = listOf(user2)
+                it[userSet] = setOf(user2)
+                it[userArray] = arrayOf(user1, user2)
+                it[stringList] = stringsWithApostrophes
+            }
+
+            assertEqualLists(listOf(id1), selectIdWhere { iterables.userList.contains(listOf(user1)) })
+            assertEqualLists(listOf(id2), selectIdWhere { iterables.userSet.contains(setOf(user2)) })
+            assertEqualLists(listOf(id1, id2), selectIdWhere { iterables.userArray.contains(arrayOf(user1, user2)) })
+            assertEqualLists(listOf(id2), selectIdWhere { iterables.stringList.contains(stringsWithApostrophes) })
         }
     }
 
