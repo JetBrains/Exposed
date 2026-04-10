5497e703064d7cd410a6448f3be82cbaa897a0ef
Oleg Babichev <oleg.babichev@jetbrains.com>
2026-04-10 10:52:18 +0200

    feat: review issues

--- exposed-dao-r2dbc/src/main/kotlin/org/jetbrains/exposed/r2dbc/dao/LongEntity.kt
+++ exposed-dao-r2dbc/src/main/kotlin/org/jetbrains/exposed/r2dbc/dao/LongEntity.kt
@@ -3,9 +3,9 @@ package org.jetbrains.exposed.r2dbc.dao
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
 import org.jetbrains.exposed.v1.core.dao.id.IdTable
 
-abstract class R2dbcLongEntity(id: EntityID<Long>) : R2dbcEntity<Long>(id)
+abstract class LongR2dbcEntity(id: EntityID<Long>) : R2dbcEntity<Long>(id)
 
-abstract class R2dbcLongEntityClass<out E : R2dbcLongEntity>(
+abstract class LongR2dbcEntityClass<out E : LongR2dbcEntity>(
     table: IdTable<Long>,
     entityType: Class<E>? = null,
     entityCtor: ((EntityID<Long>) -> E)? = null
