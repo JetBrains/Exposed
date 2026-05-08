de7e3df56c560edc0f13ad2138079bb557358199
Chantal Loncle <82039410+bog-walk@users.noreply.github.com>
2026-05-08 01:03:26 -0400

    feat: EXPOSED-1023 Extend VECTOR data type to other supporting databases
    - Merge with current implementation

--- exposed-core/src/main/kotlin/org/jetbrains/exposed/v1/core/vendors/OracleVectorExtensions.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.exposed.v1.core.vendors
-
-import org.jetbrains.exposed.v1.core.Column
-import org.jetbrains.exposed.v1.core.Table
-
-fun Table.vector(name: String, dimensions: Int): Column<FloatArray> =
-    registerColumn(name, OracleVectorColumnType(dimensions))
