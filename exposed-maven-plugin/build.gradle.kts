f9b021c4c794b487c9c9298859d9e17b87fbbc51
Frederik Pietzko <frederik.pietzko@jetbrains.com>
2026-07-21 20:12:03 +0200

    chore: fix detekt and exlude ProcessTests from normal test execution

--- exposed-maven-plugin/build.gradle.kts
+++ exposed-maven-plugin/build.gradle.kts
@@ -96,6 +96,17 @@ tasks.withType<Test>().configureEach {
 
     useJUnitPlatform()
 }
+
+tasks.test {
+    exclude("**/*ProcessTest.class")
+}
+
+val testProcess by tasks.registering(Test::class) {
+    group = "verification"
+    description = "Runs test classes ending in ProcessTest."
+
+    include("**/*ProcessTest.class")
+}
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.compilerOptions {
     freeCompilerArgs.set(listOf("-XXLanguage:+ContextParameters"))
