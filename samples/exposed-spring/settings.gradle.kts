31d4a8f9db3c13ed54a3eaf73ea2f00c47b2d802
leonid.stashevsky <leonid.stashevsky@gmail.com>
2026-08-04 12:17:34 +0200

    fix: Fix version catalog docs and versions

--- samples/exposed-spring/settings.gradle.kts
+++ samples/exposed-spring/settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("exposedLibs") {
-            from("org.jetbrains.exposed:exposed-version-catalog:1.3.2")
+            from("org.jetbrains.exposed:exposed-version-catalog:1.3.1")
         }
     }
 }
