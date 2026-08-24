209e799fc20917035dfa67bea8a4b2633f2bff23
Oleg Babichev <oleg.babichev@jetbrains.com>
2026-08-24 13:35:42 +0200

    feat: Exclude exposed-dao-r2dbc-tests from the version catalog

--- exposed-version-catalog/build.gradle.kts
+++ exposed-version-catalog/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.exposed.gradle.publishesMavenArtifact
-
 plugins {
     `version-catalog`
     alias(libs.plugins.maven.publish)
@@ -7,12 +5,20 @@ plugins {
 
 group = "org.jetbrains.exposed"
 
+val excludedFromCatalog = setOf(
+    "exposed-tests",
+    "exposed-r2dbc-tests",
+    "exposed-jdbc-r2dbc-tests",
+    "exposed-dao-r2dbc-tests",
+    "exposed-version-catalog",
+)
+
 catalog {
     versionCatalog {
         version("exposed", project.version.toString())
         rootProject.subprojects
-            .filter { it.name != project.name && it.publishesMavenArtifact() }
             .map { it.name }
+            .filter { it !in excludedFromCatalog }
             .sorted()
             .forEach { moduleName ->
                 val alias = moduleName.removePrefix("exposed-")
